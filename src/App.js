1b540b6c7d17b827bb908c3b246b8e2c3d77ec7c
Masoud/ <masoud_tesna@live.com>
2021-04-14 02:50:57 +0430

    initial install Package.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
+import 'antd/dist/antd.less';
+import './styles/custom.css';
+import { ConfigProvider , Row, Col } from 'antd';
+
+import enUS from 'antd/lib/locale/en_US';
+import faIR from 'antd/lib/locale/fa_IR';
+
+import { Layout } from 'antd';
+
 function App() {
+
+  const { Header, Footer, Content } = Layout;
+
   return (
-    <>
-      <span className="test-class App-link">App Component</span>
-    </>
+      <Layout>
+        <Header>Header</Header>
+        <Content className="px-2">
+          <Layout>
+            <Row>
+              <Col xs={24}>col</Col>
+            </Row>
+            <Row>
+              <Col xs={24} sm={12}>col-12</Col>
+              <Col xs={24} sm={12}>col-12</Col>
+            </Row>
+            <Row>
+              <Col xs={24} sm={12}>col-8</Col>
+              <Col xs={24} sm={12}>col-8</Col>
+              <Col xs={24} sm={12}>col-8</Col>
+              <Col xs={24} sm={12}>col-8</Col>
+            </Row>
+          </Layout>
+        </Content>
+        <Footer>Footer</Footer>
+      </Layout>
   );
 }
 
