21689c9621aa51eb15c209571f82a06e8ec1d4e1
Masoud/ <masoud_tesna@live.com>
2021-04-22 02:30:18 +0430

    Top Panel Default Design.

--- src/layouts/topPanel/defautl/DefaultTopPanel.js
+++ src/layouts/topPanel/defautl/DefaultTopPanel.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
+// import Styles For default:
+import './styles.less';
+
+// Ant Design Import:
+import { Row, Col, Divider } from 'antd';
+
 const DefaultTopPanel = () => {
   return (
-    <>
-      Default Top Panel!
-    </>
+    <Row className="bg-top-panel topPanel--container">
+      <Col span={24} className="topPanel--col">
+        <i className="icon-vv-all-category" />
+        <Divider type="vertical" className="bg-a5"/>
+        <a href="#">Link</a>
+        <Divider type="vertical" />
+        <a href="#">Link</a>
+      </Col>
+    </Row>
   );
 };
 
