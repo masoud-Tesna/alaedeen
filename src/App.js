f766697320521293cf58c0eec3e50fa91c634057
Masoud/ <masoud_tesna@live.com>
2021-04-21 18:51:58 +0430

    config layout folder.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
 import './styles/custom.less';
-import {ConfigProvider, Row, Col, Space, Button} from 'antd';
-import Icon, {FastBackwardOutlined, LeftCircleTwoTone, MessageOutlined, SearchOutlined} from '@ant-design/icons';
 
-import { Layout } from 'antd';
+import { Layout, Menu, Breadcrumb } from 'antd';
 
 function App() {
 
   const { Header, Footer, Content } = Layout;
 
   return (
-      <Layout>
-        <Header>Header</Header>
-        <Content className="px-2">
-          <Layout>
-            <Row className="px-4" gutter={[16, 16]}>
-              <Col xs={24} className="primary"><Button type="primary" icon={<LeftCircleTwoTone spin={true} twoToneColor="#eb2f96" />} shape="circle" size="large" /></Col>
-
-              <Col xs={24} className="mt-5">
-                <MessageOutlined style={{ fontSize: '16px', color: '#08c' }} />
-                <br />
-                <Icon type="message" style={{ fontSize: '16px', color: '#08c' }} theme="outlined" />
-              </Col>
-            </Row>
-          </Layout>
-
-        </Content>
-        <Footer>Footer</Footer>
-      </Layout>
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+          <Menu.Item key="1">nav 1</Menu.Item>
+          <Menu.Item key="2">nav 2</Menu.Item>
+          <Menu.Item key="3">nav 3</Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <div className="site-layout-content">Content</div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
   );
 }
 
