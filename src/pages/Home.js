b4c8a4c47e9d04556d69e1b2656732e1716e7ad0
Masoud/ <masoud_tesna@live.com>
2021-04-24 01:55:26 +0430

    Header Default overflow-x problem is FIXED.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,26 @@
 import React from 'react';
 
+// import Design:
+import { Col, Row } from "antd";
+
+// import blocks:
+/*import CategoriesMultiColumn from "../layouts/blocks/categories/CategoriesMultiColumn";*/
+
 const Home = () => {
   return (
-    <>Home</>
+    <Row >
+      <Col span={6}>
+        test
+      </Col>
+
+      <Col span={12}>
+        test
+      </Col>
+
+      <Col span={6}>
+        test
+      </Col>
+    </Row>
   );
 };
 
