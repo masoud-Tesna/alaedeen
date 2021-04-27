ade06a08eff0b0cc503595f0bb4a3b673c85acc0
Masoud/ <masoud_tesna@live.com>
2021-04-28 00:41:17 +0430

    Top Panel FIX!

--- src/layouts/topPanel/defautl/DefaultTopPanel.js
+++ src/layouts/topPanel/defautl/DefaultTopPanel.js
@@ -11,8 +11,8 @@ const DefaultTopPanel = () => {
   return (
     <Row className="bg-top-panel topPanel--container">
       <Col span={24} className="topPanel--col">
-        <Row className="h-100" gutter={24}>
-          <Col className="topPanel--content__left my-auto" span={ 16 }>
+        <Row className="h-100" gutter={24} justify="space-between">
+          <Col className="topPanel--content__left my-auto">
             <Space size="middle">
               <Categories userClass="categories--dropDown topPanel--content__item hover" />
               <Divider type="vertical" className="border-70"/>
@@ -33,7 +33,7 @@ const DefaultTopPanel = () => {
               </a>
             </Space>
           </Col>
-          <Col className="topPanel--content__right my-auto" span={ 8 }>
+          <Col className="topPanel--content__right my-auto">
             <Space size={0.5}>
               <a className="topPanel--content__item hover">
                 <span className="topPanel--item__text">
