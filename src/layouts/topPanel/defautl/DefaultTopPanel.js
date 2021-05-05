7f55d72e79a28cd0ce59b8434ffe6b45b1c2959e
Masoud/ <masoud_tesna@live.com>
2021-05-05 11:44:14 +0430

    Start Responsive for XS Size

--- src/layouts/topPanel/defautl/DefaultTopPanel.js
+++ src/layouts/topPanel/defautl/DefaultTopPanel.js
@@ -1,22 +1,46 @@
+import { useState } from "react";
+
 // import Styles For default:
 import './styles.less';
 
 // Ant Design Import:
-import { Row, Col, Divider, Space, Menu, Dropdown } from 'antd';
+import { Row, Col, Divider, Space, Menu, Dropdown, Button, Modal } from 'antd';
 
 // import categories drop down:
 import { CategoriesDropDownVertical as Categories } from "../../blocks/categories/CategoriesDropDownVertical";
+
+// import images used:
+import hornLogo from '../../../assets/images/logoXs.png';
 
 const handleLinkClick = (url, target) => {
   window.open(url, target)
 }
 
 const DefaultTopPanel = () => {
+
+  const [visibleTopPanelMenuXs, setVisibleTopPanelMenuXs] = useState(false);
+
+  const showTopPanelMenuXs = () => {
+    setVisibleTopPanelMenuXs(true);
+  }
+  const closeTopPanelMenuXs = () => {
+    setVisibleTopPanelMenuXs(false);
+  }
   return (
     <Row className="bg-top-panel topPanel--container">
+      <Modal
+        visible={visibleTopPanelMenuXs}
+        onCancel={closeTopPanelMenuXs}
+        className="bg-white shadow-lg w-75 m-0 p-0 topPanel--menuXs"
+        closable={false}
+        footer={null}
+      >
+        test
+      </Modal>
+
       <Col span={24} className="topPanel--col">
         <Row className="h-100" gutter={24} justify="space-between">
-          <Col className="topPanel--content__left my-auto">
+          <Col className="topPanel--content__left my-auto d-none d-lg-block">
             <Space size="middle">
               <Categories userClass="categories--dropDown topPanel--content__item hover" />
               <Divider type="vertical" className="border-70"/>
@@ -37,7 +61,7 @@ const DefaultTopPanel = () => {
               </a>
             </Space>
           </Col>
-          <Col className="topPanel--content__right my-auto">
+          <Col className="topPanel--content__right my-auto d-none d-lg-block">
             <Space size={0.5}>
               <a className="topPanel--content__item hover">
                 <span className="topPanel--item__text">
@@ -70,6 +94,15 @@ const DefaultTopPanel = () => {
               </a>
 
             </Space>
+          </Col>
+          <Col className="d-lg-none my-auto vv-cursor-pointer topPanel--col__logoXS" onClick={() => { handleLinkClick('/home', '_parent') }}>
+            <img src={hornLogo} alt="Horn"/>
+          </Col>
+          <Col className="d-lg-none my-auto topPanel--col__btnRequest">
+            <Button className="border border-secondary-2 bg-secondary-2 border-w-2 text-white font-weight-600 p-0" size="large">Request a Quote</Button>
+          </Col>
+          <Col className="d-lg-none my-auto vv-cursor-pointer" onClick={showTopPanelMenuXs}>
+            <i className="fas fa-grip-lines cursor-pointer text-white display-3" />
           </Col>
         </Row>
       </Col>
