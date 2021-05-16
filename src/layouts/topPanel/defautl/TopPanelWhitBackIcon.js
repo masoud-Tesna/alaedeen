b881e2ba05dd26006606e889f327dfea4fb56f17
Masoud/ <masoud_tesna@live.com>
2021-05-16 17:52:36 +0430

    Factories Page Design (Mobile Version 1) DONE.

--- src/layouts/topPanel/defautl/TopPanelWhitBackIcon.js
+++ src/layouts/topPanel/defautl/TopPanelWhitBackIcon.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
 // import Styles For TopPanelWhitBackIcon:
@@ -13,12 +14,61 @@ const TopPanelWhitBackIcon = () => {
   const goBack = () => {
     history.goBack()
   }
+
+  const [scrolled, setScrolled] = useState(false);
+
+  const [widthPage, setWidthPage] = useState();
+
+  const handleScroll = () => {
+    const offsetY = window.scrollY;
+
+    if (widthPage <= 576) { // if WidthPage state value <= 576 change condition for scroll and set class name
+      if(offsetY > 150 ){
+        setScrolled(true);
+      }
+      else{
+        setScrolled(false);
+      }
+    }
+    else if (widthPage >= 577) { // if WidthPage state value >= 577 change condition for scroll and set class name
+      if(offsetY > 30 ){
+        setScrolled(true);
+      }
+      else{
+        setScrolled(false);
+      }
+    }
+    else if (widthPage >= 769) { // if WidthPage state value >= 769 change condition for scroll and set class name
+      if(offsetY > 30 ){
+        setScrolled(true);
+      }
+      else{
+        setScrolled(false);
+      }
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll); //if Scroll Page Run handleScroll function
+
+    window.addEventListener('load', () => { setWidthPage(window.innerWidth);}); //if Load Page Update widthPage State Value
+    window.addEventListener('resize', () => { setWidthPage(window.innerWidth);}); //if Resize Page Update widthPage State Value
+  })
+
+  let x=[''];
+  if(scrolled){
+    x.push('scrolled');
+  }
+
   return (
-    <Row className="bg-top-panel topPanel--container">
+    <Row className={ `bg-top-panel topPanel--container TopPanelWhitBackIcon ${x.join(" ")}` }>
       <Col span={24} className="topPanel--col">
-        <Row className="h-100" gutter={24} justify="space-between">
+        <Row className="h-100" gutter={24}>
           <Col className="d-lg-none my-auto vv-cursor-pointer topPanel--col__logoXS" onClick={() => { goBack() }}>
             <i className="far fa-long-arrow-left text-white display-4" />
+          </Col>
+          <Col className="d-lg-none my-auto text-white vv-font-size-2 font-weight-bold font-italic topPanel--col__titleXS">
+            Premium OEM Factories
           </Col>
         </Row>
       </Col>
