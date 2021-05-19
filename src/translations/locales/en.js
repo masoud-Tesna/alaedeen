ba5728ee341e441bd62b31a1802fb8c115058579
Masoud/ <masoud_tesna@live.com>
2021-05-19 17:26:25 +0430

    install react-i18next and i18next create files config.. and add fa & en translate file...

---
+++ src/translations/locales/en.js
@@ -0,0 +1,6 @@
+const en = {
+  hello: "Hello",
+  system_language: 'System Language'
+};
+
+export default en;
---
+++ src/translations/locales/fa.js
@@ -0,0 +1,6 @@
+const fa = {
+  hello: "سلام",
+  system_language: "زبان سیستم"
+};
+
+export default fa;
