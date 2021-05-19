f3668218fb522a8fe6fb117388d8beec4bd38493
Masoud/ <masoud_tesna@live.com>
2021-05-20 02:14:15 +0430

    FIX: add ge language state function and change language dispatch... and another Fix...

--- src/contexts/language/LanguageContext.js
+++ src/contexts/language/LanguageContext.js
@@ -1,20 +1,10 @@
 import { createContext, useContext, useReducer } from "react";
 
-// import custom hooks:
-import { fn_set_initial_language, fn_set_local_storage } from '../../functions/Helper';
+// import language reducer:
+import { LanguageReducer } from './LanguageReducer';
 
-// import language actions and action creator:
-import { CHANGE_LANGUAGE } from './LanguageActions';
-
-// import i18n in translations:
-import i18n from "../../translations";
-
-
-
-// Initial State:
-const InitialLanguageState = {
-  language: fn_set_initial_language("lang", "en")
-};
+// import language initial state:
+import { LanguageInitialState } from './LanguageInitialState';
 
 // Language Context Create:
 const languageContext = createContext(undefined);
@@ -22,8 +12,8 @@ const languageContext = createContext(undefined);
 // create Language Context Provide:
 function LanguageProvider({ children }) {
   const [lang, dispatch] = useReducer(
-    useLanguageReducer,
-    InitialLanguageState
+    LanguageReducer,
+    LanguageInitialState
   );
 
   const language = lang.language;
@@ -35,31 +25,21 @@ function LanguageProvider({ children }) {
   );
 }
 
-// get language context and show...
-function useLanguageState() {
-  return useContext(languageContext);
+// get current language
+function useGetLanguageState() {
+  const language = useContext(languageContext).language
+  return { language };
 }
 
-function useLanguageReducer(state, action) {
-  switch (action.type) {
-    case CHANGE_LANGUAGE:
-      i18n
-        .changeLanguage(action.payload)
-        .then(() => {
-          fn_set_local_storage("lang", action.payload);
-        });
-      return {
-        language: action.payload
-      };
-    default:
-      return state;
-  }
+function useDispatchLanguageState() {
+  const languageDispatch = useContext(languageContext).dispatch
+  return { languageDispatch };
 }
 
 export { changeLanguageAction } from './LanguageActionCreators';
 
 export {
   LanguageProvider,
-  useLanguageState,
-  useLanguageReducer
+  useGetLanguageState,
+  useDispatchLanguageState
 };
