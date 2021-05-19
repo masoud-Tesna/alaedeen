93b4d258b1773e7010ee19e6854a7b815c4be84f
Masoud/ <masoud_tesna@live.com>
2021-05-19 21:08:57 +0430

    create fn_set_initial_language helper function...

--- src/functions/Helper.js
+++ src/functions/Helper.js
@@ -1,3 +1,6 @@
+// import i18n in translations:
+import i18n from "../translations";
+
 function fn_stripHtml(strip) {
   const regex = /(<([^>]+)>)/ig;
   return strip.replace(regex, '');
@@ -6,4 +9,19 @@ function fn_stripHtml(strip) {
 function fn_handleLinkClick(url, target) {
   return window.open(url, target);
 }
-export { fn_stripHtml, fn_handleLinkClick }
+
+function fn_set_initial_language(key, lang) {
+  let storageLang;
+  // Get from local storage by key
+  const item = window.localStorage.getItem(key);
+  if (item) {
+    storageLang = item;
+    i18n.changeLanguage(item);
+  } else {
+    window.localStorage.setItem(key, JSON.stringify(lang));
+    i18n.changeLanguage(key);
+  }
+  return { storageLang };
+}
+
+export { fn_stripHtml, fn_handleLinkClick, fn_set_initial_language }
