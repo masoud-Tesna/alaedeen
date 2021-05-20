8c9dc1523154107716e22ffd63b427ef1eb5de68
Masoud/ <masoud_tesna@live.com>
2021-05-21 03:02:00 +0430

    Stats.js: get api and show -> total store, total products and total user (DONE!)

--- src/layouts/blocks/static_templates/Stats.js
+++ src/layouts/blocks/static_templates/Stats.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 //import Style:
 import './styles/Stats.less';
@@ -6,21 +6,26 @@ import './styles/Stats.less';
 // import ANT Design Components Used:
 import { Col, Row } from "antd";
 
+// import custom hooks:
+import { useGetApi } from "../../../functions";
+import axios from "axios";
+
 const Stats = () => {
+  const { items } = useGetApi('https://hornb2b.com/stats-api', 'stats', false);
   return (
     <div className="stats--container">
       <Row className="h-100 stats--content" justify="space-around" align="middle">
         <Col className="text-center text-white" span={5}>
           <div className="stats--content__title font-weight-600 text-shadow">Registered Users</div>
-          <div className="stats--content__value font-weight-bold text-shadow">1527</div>
+          <div className="stats--content__value font-weight-bold text-shadow">{ items.total_user }</div>
         </Col>
         <Col className="text-center text-white" span={5}>
           <div className="stats--content__title font-weight-600 text-shadow">Total Products</div>
-          <div className="stats--content__value font-weight-bold text-shadow">1646</div>
+          <div className="stats--content__value font-weight-bold text-shadow">{ items.total_products }</div>
         </Col>
         <Col className="text-center text-white" span={5}>
           <div className="stats--content__title font-weight-600 text-shadow">Total Stores</div>
-          <div className="stats--content__value font-weight-bold text-shadow">26</div>
+          <div className="stats--content__value font-weight-bold text-shadow">{ items.total_store }</div>
         </Col>
       </Row>
     </div>
