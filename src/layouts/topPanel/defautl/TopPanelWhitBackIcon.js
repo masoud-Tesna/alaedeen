7a481684169ca281b95cf2f610f6d49335dd1653
Masoud/ <masoud_tesna@live.com>
2021-05-27 01:23:14 +0430

    Top Panel FIX...

--- src/layouts/topPanel/defautl/TopPanelWhitBackIcon.js
+++ src/layouts/topPanel/defautl/TopPanelWhitBackIcon.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useHistory } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 
 // import Styles For TopPanelWhitBackIcon:
 import './styles.less';
@@ -8,60 +8,15 @@ import './styles.less';
 import { Row, Col } from 'antd';
 
 
-const TopPanelWhitBackIcon = () => {
+const TopPanelWhitBackIcon = ({ scrolledClass }) => {
   const history = useHistory()
 
   const goBack = () => {
     history.goBack()
   }
 
-  const [scrolled, setScrolled] = useState(false);
-
-  const [widthPage, setWidthPage] = useState();
-
-  const handleScroll = () => {
-    const offsetY = window.scrollY;
-
-    if (widthPage <= 576) { // if WidthPage state value <= 576 change condition for scroll and set class name
-      if(offsetY > 150 ){
-        setScrolled(true);
-      }
-      else{
-        setScrolled(false);
-      }
-    }
-    else if (widthPage >= 577) { // if WidthPage state value >= 577 change condition for scroll and set class name
-      if(offsetY > 30 ){
-        setScrolled(true);
-      }
-      else{
-        setScrolled(false);
-      }
-    }
-    else if (widthPage >= 769) { // if WidthPage state value >= 769 change condition for scroll and set class name
-      if(offsetY > 30 ){
-        setScrolled(true);
-      }
-      else{
-        setScrolled(false);
-      }
-    }
-  }
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll); //if Scroll Page Run handleScroll function
-
-    window.addEventListener('load', () => { setWidthPage(window.innerWidth);}); //if Load Page Update widthPage State Value
-    window.addEventListener('resize', () => { setWidthPage(window.innerWidth);}); //if Resize Page Update widthPage State Value
-  })
-
-  let x=[''];
-  if(scrolled){
-    x.push('scrolled');
-  }
-
   return (
-    <Row className={ `bg-top-panel topPanel--container TopPanelWhitBackIcon ${x.join(" ")}` }>
+    <Row className={ `bg-top-panel topPanel--container TopPanelWhitBackIcon ${scrolledClass}` }>
       <Col span={24} className="topPanel--col">
         <Row className="h-100" gutter={24}>
           <Col className="d-lg-none my-auto vv-cursor-pointer topPanel--col__logoXS" onClick={() => { goBack() }}>
--- src/layouts/topPanel/index.js
+++ src/layouts/topPanel/index.js
@@ -19,6 +19,52 @@ const TopPanel = () => {
   // Set State For Path NAme:
   const [pathName, setPathName] = useState(location.pathname);
 
+  const [scrolled, setScrolled] = useState(false);
+  const [scrolledClass, setScrolledClass] = useState();
+
+  const [widthPage, setWidthPage] = useState();
+
+  const handleScroll = () => {
+    const offsetY = window.scrollY;
+
+    if (widthPage <= 576) { // if WidthPage state value <= 576 change condition for scroll and set class name
+      if(offsetY > 150 ){
+        setScrolled(true);
+      }
+      else{
+        setScrolled(false);
+      }
+    }
+    else if (widthPage >= 577) { // if WidthPage state value >= 577 change condition for scroll and set class name
+      if(offsetY > 30 ){
+        setScrolled(true);
+      }
+      else{
+        setScrolled(false);
+      }
+    }
+    else if (widthPage >= 769) { // if WidthPage state value >= 769 change condition for scroll and set class name
+      if(offsetY > 30 ){
+        setScrolled(true);
+      }
+      else{
+        setScrolled(false);
+      }
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll); //if Scroll Page Run handleScroll function
+
+    window.addEventListener('load', () => { setWidthPage(window.innerWidth);}); //if Load Page Update widthPage State Value
+    window.addEventListener('resize', () => { setWidthPage(window.innerWidth);}); //if Resize Page Update widthPage State Value
+    if(scrolled){
+      setScrolledClass('scrolled');
+    }else {
+      setScrolledClass('')
+    }
+  }, [handleScroll]);
+
   // if Changed Location.pathname Change state:
   useEffect(() => {
     setPathName(location.pathname);
@@ -26,7 +72,7 @@ const TopPanel = () => {
 
   // Check pathName For Set Default Top Panel Or Not:
   if (pathName === '/factories' && width <= 991) {
-    return <TopPanelWhitBackIcon />
+    return <TopPanelWhitBackIcon scrolledClass={scrolledClass} />
   }
 
   // if get default Header:
