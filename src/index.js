66ec11f698789967d043cd2d4f4aa365ace51c2a
Masoud/ <masoud_tesna@live.com>
2021-06-03 01:13:01 +0430

    FIX...

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 // import i18n:
-import './translations';
+import i18n from "./translations";
 
 // import language context:
 import { LanguageProvider } from "./contexts/language/LanguageContext";
