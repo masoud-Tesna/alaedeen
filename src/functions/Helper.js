85d927e22bc60c83958b0c90b3a1e60a43e1e1a2
Masoud/ <masoud_tesna@live.com>
2021-06-02 19:19:41 +0430

    create __() helper function for show translate...

--- src/functions/Helper.js
+++ src/functions/Helper.js
@@ -1,9 +1,50 @@
 // import i18n in translations:
 import i18n from "../translations";
+import { useTranslation } from "react-i18next";
 
 function fn_stripHtml(strip) {
   const regex = /(<([^>]+)>)/ig;
   return strip.replace(regex, '');
+}
+
+function __(world, prefix = "", sign = ".") {
+
+  const { t } = useTranslation();
+
+  let returnWord = world;
+
+  // change to string:
+  returnWord = returnWord.toString();
+
+  // trim:
+  returnWord = returnWord.trim();
+
+  // lower case:
+  returnWord = returnWord.toLowerCase();
+
+  // replace:
+  returnWord = returnWord.replaceAll("    ", "_");
+  returnWord = returnWord.replaceAll("   ", "_");
+  returnWord = returnWord.replaceAll("  ", "_");
+  returnWord = returnWord.replaceAll(" ", "_");
+  returnWord = returnWord.replaceAll(" / ", "_and_");
+  returnWord = returnWord.replaceAll("/", "_and_");
+  returnWord = returnWord.replaceAll(" & ", "_and_");
+  returnWord = returnWord.replaceAll("&", "_and_");
+  returnWord = returnWord.replaceAll("(", "");
+  returnWord = returnWord.replaceAll(")", "");
+  returnWord = returnWord.replaceAll(".", "");
+  returnWord = returnWord.replaceAll(",", "");
+  returnWord = returnWord.replaceAll("’", "");
+  returnWord = returnWord.replaceAll("²", "");
+  returnWord = returnWord.replaceAll("%", "");
+  returnWord = returnWord.replaceAll(" - ", "_");
+  returnWord = returnWord.replaceAll("-", "_");
+
+  returnWord = prefix? `${prefix}${sign}${returnWord}` : returnWord;
+
+  return t(returnWord);
+
 }
 
 function fn_handleLinkClick(url, target) {
@@ -28,4 +69,4 @@ function fn_set_local_storage(key, value) {
   window.localStorage.setItem(key, value);
 }
 
-export { fn_stripHtml, fn_handleLinkClick, fn_set_initial_language, fn_set_local_storage }
+export { fn_stripHtml, __, fn_handleLinkClick, fn_set_initial_language, fn_set_local_storage }
