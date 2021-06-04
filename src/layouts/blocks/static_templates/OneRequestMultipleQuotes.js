03cb1b872cae3c0e78d9f6f9caf63d43aedbb476
Masoud/ <masoud_tesna@live.com>
2021-06-04 10:28:04 +0430

    add translation...

--- src/layouts/blocks/static_templates/OneRequestMultipleQuotes.js
+++ src/layouts/blocks/static_templates/OneRequestMultipleQuotes.js
@@ -3,11 +3,19 @@ import React from 'react';
 // import style file:
 import './styles/OneRequestMultipleQuotes.less';
 
+// import helper functions:
+import { __ } from '../../../functions/Helper';
+
+import { useTranslation } from "react-i18next";
+
 // import ant design:
 import { Col, Row, Form, Input, Button, Select, InputNumber} from "antd";
+
 const { Option } = Select;
 
 const OneRequestMultipleQuotes = () => {
+
+  const { t } = useTranslation();
 
   const onFinish = (values) => {
     console.log(values);
@@ -21,7 +29,7 @@ const OneRequestMultipleQuotes = () => {
         onFinish={onFinish}>
         <Row className="h-100" align="middle">
           <Col span={24}>
-            <div className="font-weight-bold vv-font-size-2-2 text-black oneRequest--formContent__caption">One Request Multiple Quotes</div>
+            <div className="font-weight-bold vv-font-size-2-2 text-black oneRequest--formContent__caption">{ t(__('One Request Multiple Quotes')) }</div>
           </Col>
           <Col className="mt-2" span={24}>
             <Form.Item
@@ -32,7 +40,7 @@ const OneRequestMultipleQuotes = () => {
                   required: true,
                 },
               ]}>
-              <Input placeholder="What are you looking for... " />
+              <Input placeholder={ t(__('What are you looking for...')) } />
             </Form.Item>
           </Col>
           <Col span={24}>
@@ -46,7 +54,7 @@ const OneRequestMultipleQuotes = () => {
                       required: true,
                     },
                   ]}>
-                  <InputNumber placeholder="Quantity" />
+                  <InputNumber placeholder={ t(__('Quantity')) } min={1} />
                 </Form.Item>
               </Col>
               <Col span={12}>
@@ -60,7 +68,7 @@ const OneRequestMultipleQuotes = () => {
                   ]}
                 >
                   <Select
-                    placeholder="Piece/Pieces"
+                    placeholder={ t(__('Piece/Pieces')) }
                     allowClear
                   >
                     <Option value="piece1">Piece1</Option>
@@ -74,7 +82,7 @@ const OneRequestMultipleQuotes = () => {
           <Col className="text-center mt-2" span={24}>
             <Form.Item className="oneRequest--formContent__btn">
               <Button className="border border-secondary-2 rounded-3 bg-secondary-2 text-white vv-font-size-1 font-weight-500 p-0" size="large" htmlType="submit">
-                Request a Quote
+                { t(__('Request a Quote')) }
               </Button>
             </Form.Item>
           </Col>
