730e551fa157a07cd220594b4826c1ae6bf98870
Masoud/ <masoud_tesna@live.com>
2021-06-23 21:05:44 +0430

    show category in top panel drop down...

--- src/layouts/blocks/categories/CategoriesDropDownVertical.js
+++ src/layouts/blocks/categories/CategoriesDropDownVertical.js
@@ -1,30 +1,45 @@
 import { Dropdown, Menu } from "antd";
-import { DownOutlined } from "@ant-design/icons";
+import { AppstoreOutlined, DownOutlined, MailOutlined, SettingOutlined } from "@ant-design/icons";
 
 // import helper functions:
 import { __ } from "../../../functions/Helper";
 
 import { useTranslation } from "react-i18next";
 
-const CategoriesContent = (
-  <Menu className="menu-test">
-    <Menu.Item key="0">
-      <a href="https://www.antgroup.com">1st menu item</a>
-    </Menu.Item>
-    <Menu.Item key="1">
-      <a href="https://www.aliyun.com">2nd menu item</a>
-    </Menu.Item>
-    <Menu.Divider />
-    <Menu.Item key="3">3rd menu item</Menu.Item>
-  </Menu>
-);
+// import custom hooks:
+import { useGetApi } from "../../../functions";
+
+const CategoriesContent = () => {
+  const categories = useGetApi(`home-categories-api`, '', 'categories');
+
+  return(
+    <Menu
+      style={{ minWidth: 250 }}
+      mode="inline"
+      triggerSubMenuAction={"click"}
+    >
+      {categories.load ?
+        <>Loading...</> :
+        <>
+          {categories.items.map((category) => {
+            return(
+              <Menu.Item key={ category.category_id }>
+                <a href={ category.link }>{ category.category }</a>
+              </Menu.Item>
+            )
+          })}
+        </>
+      }
+    </Menu>
+  );
+};
 
 const CategoriesDropDownVertical = ({ userClass }) => {
 
   const { t } = useTranslation();
 
   return (
-    <Dropdown className={ userClass } overlay={CategoriesContent} trigger={['click']}>
+    <Dropdown className={ userClass } overlay={CategoriesContent()} trigger={['click']}>
       <span className="vv-cursor-pointer" onClick={e => e.preventDefault()}>
         <i className="fal fa-list-ul mr-3 vv-font-size-2" />
         <span className="topPanel--item__text">{t(__('Categories'))}</span>
