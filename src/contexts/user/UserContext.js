9537d263467578c078e18ebcb6a93e7de97774a7
Masoud/ <masoud_tesna@live.com>
2021-07-29 06:16:36 +0430

    use getUserLoginFromHornDomain FIX...

--- src/contexts/user/UserContext.js
+++ src/contexts/user/UserContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useReducer } from "react";
+import { createContext, useContext, useEffect, useReducer, useState } from "react";
 
 // import user reducer:
 import { UserReducer } from './UserReducer';
@@ -10,8 +10,10 @@ import { signInAction, logOutAction, checkSignInLoadingAction, signInLoadingFals
 import axios from "axios";
 
 import LoaderSpinner from '../../layouts/blocks/static_templates/LoadSpinner';
-import { fn_get_local_storage_with_expiry } from "../../functions/Helper";
+import { fn_get_local_storage_with_expiry, fn_set_local_storage } from "../../functions/Helper";
 import { useGetLanguageState } from "../language/LanguageContext";
+import { getUserLoginFromHornDomain } from "../../functions/accessExternalLocalStorage";
+
 
 // User Context Create:
 const userContext = createContext();
@@ -28,14 +30,18 @@ export function UserProvider ({ children }) {
     UserInitialState
   );
 
+  const [getUserLoginStatusHorn, setGetUserLoginStatusHorn] = useState("loading");
+
   useEffect(() => {
+
+    console.log('start');
 
     let mounted  = true;
 
     const checkRememberMe = localStorage.getItem('remember_me');
 
     let clientUserLoginLocalStorage,
-        clientPasswordLocalStorage;
+      clientPasswordLocalStorage;
 
     if (checkRememberMe) {
       clientUserLoginLocalStorage = localStorage.getItem('user_login');
@@ -47,18 +53,49 @@ export function UserProvider ({ children }) {
 
     if (clientUserLoginLocalStorage && clientPasswordLocalStorage) {
       dispatch(checkSignInLoadingAction());
+      console.log('start with local storage');
       signIn(clientUserLoginLocalStorage, clientPasswordLocalStorage, language)
         .then(res => {
           dispatch(signInAction(res.data.auth, clientUserLoginLocalStorage, clientPasswordLocalStorage, false));
         });
-      mounted  = false;
-      return () => mounted = false;
+
     }else {
+
+      getUserLoginFromHornDomain.get('remember_me', function (rememberMeError, rememberMe) { // get remember_me from hornb2b domain local storage
+        if (rememberMe) { // if remember_me is true:
+          fn_set_local_storage("remember_me", 'true'); // set remember_me in to local storage
+          //setGetUserLoginStatusHorn('get remember_me'); // set userLogin status
+        }
+
+        getUserLoginFromHornDomain.get('user_login', function (userLoginError, userLogin) { // get user_login from hornb2b domain local storage
+          if (userLogin) {
+
+            fn_set_local_storage("user_login", userLogin); // set user_login in to local storage
+            //setGetUserLoginStatusHorn('get user_login'); // set userLogin status
+
+            getUserLoginFromHornDomain.get('user_password', function (userPasswordError, userPassword) { // get user_password from hornb2b domain local storage
+              if (userPassword) {
+
+                fn_set_local_storage("user_password", userPassword); // set user_password in to local storage
+                setGetUserLoginStatusHorn('get user_password'); // set userLogin status
+
+              }
+            });
+
+          }
+        });
+
+      });
+
+    }
+
+    return () => {
+      mounted = false;
       dispatch(signInLoadingFalseAction());
     }
 
-    return () => mounted = false;
-  }, [language]);
+  }, [language, getUserLoginStatusHorn]);
+
 
   return (
     <userContext.Provider value={{ auth, dispatch }}>
@@ -83,11 +120,20 @@ export async function logout(dispatch) {
   dispatch(checkSignInLoadingAction());
 
   signOutAxios()
-    .then(res => {
+    .then(() => {
       dispatch(logOutAction());
       window.localStorage.removeItem('user_login');
       window.localStorage.removeItem('user_password');
       window.localStorage.removeItem('remember_me');
+    })
+    .then(() => {
+      getUserLoginFromHornDomain.remove('user_login', function(userLoginErrorRemove, userLoginRemove) {
+        getUserLoginFromHornDomain.remove('user_password', function(userPasswordErrorRemove, userPasswordRemove) {
+          getUserLoginFromHornDomain.remove('remember_me', function(rememberMeErrorRemove, rememberMeRemove) {
+            // foo is now removed
+          });
+        });
+      });
     });
 
 }
