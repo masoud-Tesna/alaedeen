ba31c09be9f6e366217856e5d98f80b18eac3de5
Masoud/ <masoud_tesna@live.com>
2021-08-01 04:36:25 +0430

    install react-query and set initial settings...

--- src/index.js
+++ src/index.js
@@ -1,27 +1,44 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
 // import i18n:
 import "./translations";
 
+// import Cookie Provider:
+import { CookiesProvider } from "react-cookie";
+
+// import react query provider:
+import { QueryClient, QueryClientProvider } from "react-query";
+
+// import react query dev tools:
+import { ReactQueryDevtools } from "react-query/devtools";
+
 // import language context:
 import { LanguageProvider } from "./contexts/language/LanguageContext";
 
+// import user  context:
 import { UserProvider } from "./contexts/user/UserContext";
 
-/*import * as serviceWorker from './serviceWorker';*/
+// import Application:
+import App from './App';
+
+// initial query client:
+const queryClient = new QueryClient();
+
 
 const Application = () => {
   return (
-    <LanguageProvider>
-      <UserProvider>
-        <App />
-      </UserProvider>
-    </LanguageProvider>
+    <CookiesProvider>
+      <QueryClientProvider client={queryClient}>
+        <LanguageProvider>
+          <UserProvider>
+            <App />
+            <ReactQueryDevtools />
+          </UserProvider>
+        </LanguageProvider>
+      </QueryClientProvider>
+    </CookiesProvider>
   );
 }
 
 ReactDOM.render( <Application />, document.getElementById('HornApp'));
 
-/*serviceWorker.register();*/
