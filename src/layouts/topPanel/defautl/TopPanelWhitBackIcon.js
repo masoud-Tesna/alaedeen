5a27583f6bb6faca015bdd6d2b8d8e2f270c2fac
Masoud/ <masoud_tesna@live.com>
2021-09-05 02:16:57 +0430

    fix style...

--- src/layouts/topPanel/defautl/TopPanelWhitBackIcon.js
+++ src/layouts/topPanel/defautl/TopPanelWhitBackIcon.js
@@ -21,8 +21,8 @@ const TopPanelWhitBackIcon = ({ scrolledClass, pathName }) => {
   return (
     <Row className={ `bg-top-panel topPanel--container TopPanelWhitBackIcon ${scrolledClass}` }>
       <Col span={24} className="topPanel--col">
-        <Row className="h-100" gutter={24}>
-          <Col className="d-lg-none my-auto vv-cursor-pointer topPanel--col__logoXS" onClick={() => { goBack() }}>
+        <Row className="h-100">
+          <Col className="my-auto vv-cursor-pointer pr-4 topPanel--col__logoXS" onClick={() => { goBack() }}>
             <i className="far fa-long-arrow-left text-white display-4" />
           </Col>
 
