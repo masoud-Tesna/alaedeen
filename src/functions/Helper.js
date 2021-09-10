e3d74d36e4ba8f5eb1ceaa12842ee5f185b84601
Masoud/ <masoud_tesna@live.com>
2021-09-10 15:13:35 +0430

    add to function for array: split and partition...

--- src/functions/Helper.js
+++ src/functions/Helper.js
@@ -47,3 +47,30 @@ export const useParsPathName = () => {
   const pathNameExplode = (window.location.pathname.split("/").slice(1));
   return pathNameExplode[ 0 ] || 'homePage';
 }
+
+// split array for example: $array=[1,2,3,4,5,6,7] splitArray($array, 2) = [[1,2],[3,4],[5,6],[7]] => 2-item arrays in an array :
+export const splitArray = (arr, len) => {
+  let chunks = [], i = 0, n = arr.length;
+  while (i < n) {
+    chunks.push(arr.slice(i, i += len));
+  }
+  return chunks;
+}
+
+// partition array for example: $array=[1,2,3,4,5,6,7] splitArray($array, 2) = [[1,2,3,4],[5,6,7]] => Items in two arrays:
+export const partitionArray = (list = [], n = 1) => {
+  const isPositiveInteger = Number.isSafeInteger(n) && n > 0;
+  if (!isPositiveInteger) {
+    throw new RangeError('n must be a positive integer');
+  }
+
+  const partitions = [];
+  const partitionLength = Math.ceil(list.length / n);
+
+  for (let i = 0; i < list.length; i += partitionLength) {
+    const partition = list.slice(i, i+partitionLength);
+    partitions.push( partition );
+  }
+
+  return partitions;
+}
