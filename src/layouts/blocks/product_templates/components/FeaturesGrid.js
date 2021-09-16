10b06d3992e960a67a7986188dfd854dc99b4492
Masoud <masoud_tesna@live.com>
2021-09-16 17:00:19 +0430

    feature grid level1 done...

--- src/layouts/blocks/product_templates/components/FeaturesGrid.js
+++ src/layouts/blocks/product_templates/components/FeaturesGrid.js
@@ -3,22 +3,38 @@ import { Fragment } from 'react';
 // import style file:
 import './styles/FeaturesGrid.less';
 import {Col, Row} from "antd";
+import {useTranslation} from "react-i18next";
+import {__} from "../../../../functions/Helper";
 
 const FeaturesGrid = (props) => {
+
+    const { t } = useTranslation();
 
     const { features } = props;
 
     return (
-        <Row className="row-cols-2" gutter={[16, 30]}>
+        <Row className="row-cols-4" gutter={[7, 15]}>
             {(features && features?.length !== 0) && Object.entries(features).slice(0, 6).map(([key, feature]) => {
                 return (
                     <Fragment key={`features_${key}`}>
                         <Col className="features--variant">
-                            { feature?.description } { feature?.suffix && `(${feature?.suffix})` }
+                            { t(__(feature?.description)) } { feature?.suffix && `(${feature?.suffix})` }
                         </Col>
 
                         <Col className="features--value">
-                            { feature?.value && feature?.value }
+                            { feature?.value && t(__(feature?.value)) }
+
+                            { feature?.value_int && parseFloat(feature?.value_int).toFixed(0) }
+
+                            { (feature?.variants && feature?.variants?.length !== 0) &&
+                                Object.entries(feature?.variants).map(([key, variant]) => {
+                                    return (
+                                        <span key={`features_variants_${key}`}>
+                                            { t(__(variant?.variant)) }
+                                        </span>
+                                    );
+                                })
+                            }
                         </Col>
                     </Fragment>
                 );
