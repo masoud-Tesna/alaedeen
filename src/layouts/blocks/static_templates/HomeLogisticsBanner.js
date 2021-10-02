ff3453ded94be9424ca896125d602eed9fe5ab23
Masoud/ <masoud_tesna@live.com>
2021-10-03 00:25:33 +0330

    clean code: remove unused component or code...

--- src/layouts/blocks/static_templates/HomeLogisticsBanner.js
+++ src/layouts/blocks/static_templates/HomeLogisticsBanner.js
@@ -1,6 +1,5 @@
 // import Style File:
 import './styles/HomeLogisticsBanner.less';
-import { Col, Row } from "antd";
 
 // import helper functions:
 import { __ } from '../../../functions/Helper';
@@ -8,8 +7,7 @@ import { useWindowSize } from "../../../functions";
 
 import { useTranslation } from "react-i18next";
 
-// import banner images
-import logisticsBannerFa from '../../../assets/images/logisticsBanner-fa.png';
+// import banner images:
 import logisticsBannerRtl from '../../../assets/images/logisticsBanner-rtl.png';
 import logisticsBannerRtlXs from '../../../assets/images/logisticsBannerXs-rtl.png';
 import logisticsBannerLtr from '../../../assets/images/logisticsBanner-ltr.png';
@@ -29,57 +27,26 @@ const HomeLogisticsBanner = () => {
     <div className="homeLogisticsBanner--container">
       <div className="homeLogisticsBanner--content h-100">
 
-        {/*for Mobile And Fa Or Ar Language*/}
-        {(width < 768 && (config.language === 'fa' || config.language === 'ar')) &&
+        {/* for Mobile And persian Or arabic Language */}
+        {(width < 992 && (config.language === 'fa' || config.language === 'ar')) &&
           <img className="homeLogisticsBanner--img" src={ logisticsBannerRtlXs } alt={ t(__('Logistics Services')) }/>
         }
 
-        {/*for Mobile And En Language*/}
-        {(width < 768 && config.language === 'en') &&
+        {/* for Mobile And English Language */}
+        {(width < 992 && config.language === 'en') &&
           <img className="homeLogisticsBanner--img" src={ logisticsBannerLtrXs } alt={ t(__('Logistics Services')) }/>
         }
 
-        {/*for Desktop And En Language*/}
-        {(width >= 768 && config.language === 'en') &&
-          <img className="homeLogisticsBanner--img" src={ logisticsBannerLtr } alt={ t(__('Logistics Services')) }/>
-        }
-
-        {/*for Desktop And Fa Language*/}
-        {/*{(width >= 768 && config.language === 'fa') &&
-         <img className="homeLogisticsBanner--img" src={ logisticsBannerFa } alt={ t(__('Logistics Services')) }/>
-        }
-*/}
-        {/*for Desktop And Ar Language*/}
+        {/* for Desktop And persian Or arabic Language */}
         {(width >= 768 && (config.language === 'ar' || config.language === 'fa')) &&
           <img className="homeLogisticsBanner--img" src={ logisticsBannerRtl } alt={ t(__('Logistics Services')) }/>
         }
 
-        {/*<Col className="homeLogisticsBanner--caption" span={11}>
-          <Row className="h-100 homeLogisticsBanner--details" align="middle">
-            Desktop
-            <Col span={24} className="d-none d-lg-block py-3 text-secondary font-weight-600 text-center vv-font-size-4 homeLogisticsBanner--caption__text1">{ t(__('Logistics')) }</Col>
-            <Col span={24} className="d-none d-lg-block px-3 py-3 text-white vv-font-size-1-2 homeLogisticsBanner--caption__text2">
-              <div className="caption--text2__top vv-font-size-2 pb-5">
-                { t(__('Worried about shipping?')) }
-              </div>
-              <div className="caption--text2__middle vv-font-size-2 pb-5">
-                { t(__('Send your goods through the most reliable shipping service providers.')) }
-              </div>
-              <div className="caption--text2__bottom vv-font-size-2">
-                { t(__(' Get the best price now!')) }
-              </div>
-            </Col>
-            Mobile
-            <Col span={24} className="d-lg-none py-0 py-lg-3 text-secondary font-weight-600 text-center vv-font-size-2-5 homeLogisticsBanner--caption__text1">{ t(__('Logistics Services')) }</Col>
-            <Col span={24} className="d-lg-none text-white font-weight-600 text-center vv-font-size-1-5 homeLogisticsBanner--caption__text1">{ t(__('Get the best price now!')) }</Col>
-            Desktop & Mobile
-            <Col span={24} className="py-3 text-center homeLogisticsBanner--caption__btn">
-              <a href="https://alaedeen.com/horn/logistics/" className="text-black border-0 d-inline-block">
-                { t(__('Go to page')) }
-              </a>
-            </Col>
-          </Row>
-        </Col>*/}
+        {/* for Desktop And English Language */}
+        {(width >= 992 && config.language === 'en') &&
+          <img className="homeLogisticsBanner--img" src={ logisticsBannerLtr } alt={ t(__('Logistics Services')) }/>
+        }
+
       </div>
     </div>
   );
