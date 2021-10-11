e776fc6446418f7a279fc63ff96342d142ef40e2
Masoud/ <masoud_tesna@live.com>
2021-10-11 07:06:33 +0330

    قسمت بالا بجز گالری تصویر تمام...

--- src/layouts/blocks/product_templates/index.js
+++ src/layouts/blocks/product_templates/index.js
@@ -2,7 +2,7 @@
 import './styles/ProductTemplate.less';
 
 // import ant design components:
-import {Button, Col, Row} from "antd";
+import { Button, Col, Row, Skeleton } from "antd";
 
 import {useWindowSize} from "../../../functions";
 import {useTranslation} from "react-i18next";
@@ -10,6 +10,7 @@ import {__} from "../../../functions/Helper";
 import FeaturesGrid from "./components/FeaturesGrid";
 import { useGetConfig } from "../../../contexts/config/ConfigContext";
 import ProductTab from "../product_tabs";
+import React from "react";
 
 const ProductTemplate = (props) => {
 
@@ -41,50 +42,67 @@ const ProductTemplate = (props) => {
                 <Col span={14}>
                   <Row className="h-100" gutter={[0, 10]}>
                     <Col span={24} className="productDetails--title">
-                      <h1>{ product?.product }</h1>
+                      {(isLoading || !product.product) ?
+                        <Skeleton.Input style={{ width: 220, height: 35 }} active={true} size={"small"} /> :
+                        <h1>{ product?.product }</h1>
+                      }
                     </Col>
 
-                    {(config.countryCode !== 'IR' && productPrice !== "0.00") &&
-                      <Col span={24} className="productDetails--price">
-                        <span className={ `${ width >= 768 ? 'vv-font-size-2-5' : 'vv-font-size-1-5' } text-primary font-weight-bold` }>
-                          ${ productPrice }
-                        </span>
-                        { productListPrice !== "0.00" &&
-                        <span className={ `${ width >= 768 ? 'vv-font-size-2-5' : 'vv-font-size-1-5' } text-primary font-weight-bold` }> - ${productListPrice}</span>
-                        }
-                        {product?.quantity_unit &&
-                        <span className={ ` vv-font-size-1-9 text-90 font-weight-600` }>
-                            / { product?.quantity_unit }
+                    {(isLoading || !product.product) ?
+                      <Skeleton.Input style={{ width: 300, height: 35 }} active={true} size={"small"} /> :
+                      (config.countryCode !== 'IR' && productPrice !== "0.00") &&
+                        <Col span={24} className="productDetails--price">
+                          <span className={ `${ width >= 768 ? 'vv-font-size-2-5' : 'vv-font-size-1-5' } text-primary font-weight-bold` }>
+                            ${ productPrice }
                           </span>
-                        }
-                        {(product?.min_qty && product?.quantity_unit) &&
-                        <span className="vv-font-size-1-9 text-90 font-weight-600">
-                            | {t(__('Minimum quantity'))}: { product?.min_qty } { product?.quantity_unit }
-                          </span>
-                        }
+
+                          { productListPrice !== "0.00" &&
+                            <span className={ `${ width >= 768 ? 'vv-font-size-2-5' : 'vv-font-size-1-5' } text-primary font-weight-bold` }> - ${productListPrice}</span>
+                          }
+
+                          {product?.quantity_unit &&
+                            <span className={ ` vv-font-size-1-9 text-90 font-weight-600` }>
+                              &nbsp; /&nbsp; { product?.quantity_unit }
+                            </span>
+                          }
+
+                          {(product?.min_qty && product?.quantity_unit) &&
+                            <span className="vv-font-size-1-9 text-90 font-weight-600">
+                              &nbsp; |&nbsp; {t(__('Minimum quantity'))}: { product?.min_qty } { product?.quantity_unit }
+                            </span>
+                          }
                       </Col>
                     }
 
                     <Col span={24} className="productDetails--getLatestPrice">
-                      { t(__('get latest price')) }
+                      {(isLoading || !product.product) ?
+                        <Skeleton.Input style={{ width: 70, height: 22 }} active={true} size={"small"} /> :
+                        t(__('get latest price'))
+                      }
                     </Col>
 
                     <Col span={24} className="productDetails--features">
-                      <FeaturesGrid features={product?.product_features} />
+                      <FeaturesGrid features={product?.product_features} isLoading={isLoading} />
                     </Col>
 
                     <Col span={24} className="productDetails--locationRequestBtn">
-                      <Row justify={"space-between"}>
+                      <Row justify={"space-between"} align="bottom">
                         <Col className="productDetails--location">
-                          {product?.wk_location &&
-                            <>
-                              <i className="fal fa-map-marker-alt text-red-a0 mr-2 mr-lg-3" />
-                              <span className="text-47">{ product.wk_location }</span>
-                            </>
+                          {(isLoading || !product.product) ?
+                            <Skeleton.Input style={{ width: 86, height: 18 }} active={true} size={"small"} /> :
+                            product?.wk_location &&
+                              <>
+                                <i className="fal fa-map-marker-alt text-red-a0 mr-2 mr-lg-3" />
+                                <span className="text-47">{ product.wk_location }</span>
+                              </>
                           }
                         </Col>
                         <Col className="productDetails--requestBtn">
-                          <Button className="border border-primary p-0 productsOneColumnVertical--item__sendRequestBtn" size="large">{t(__('send request'))}</Button>
+                          {(isLoading || !product.product) ?
+                            <Skeleton.Input style={{ width: 230, height: 50 }} active={true} size={"small"} /> :
+                            <Button className="border border-primary p-0" size="large">{t(__('send request'))}</Button>
+                          }
+
                         </Col>
                       </Row>
                     </Col>
