a0ef42bfe3d195b371aef834757c73a9df887548
Masoud/ <masoud_tesna@live.com>
2021-10-21 08:12:15 +0330

    ایجاد فانکشن appendQueryParameter() دریافت آدرس و اضافه کردن پارامتر یا آپدیت پارامتر و برگرداندن آن به صورت رشته

--- src/functions/Helper.js
+++ src/functions/Helper.js
@@ -77,3 +77,54 @@ export const partitionArray = (list = [], n = 1) => {
 
   return partitions;
 }
+
+// get url and add parameter to url or update parameter by new value (no change url! return string new url)
+export function appendQueryParameter(key, value) {
+
+  const url = window.location.href;
+
+  if(value!==undefined){
+    value = encodeURI(value);
+  }
+  var hashIndex = url.indexOf("#")|0;
+  if (hashIndex === -1) hashIndex = url.length|0;
+  var urls = url.substring(0, hashIndex).split('?');
+  var baseUrl = urls[0];
+  var parameters = '';
+  var outPara = {};
+  if(urls.length>1){
+    parameters = urls[1];
+  }
+  if(parameters!==''){
+    parameters = parameters.split('&');
+    for(k in parameters){
+      var keyVal = parameters[k];
+      keyVal = keyVal.split('=');
+      var ekey = keyVal[0];
+      var evalue = '';
+      if(keyVal.length>1){
+        evalue = keyVal[1];
+      }
+      outPara[ekey] = evalue;
+    }
+  }
+
+  if(value!==undefined){
+    outPara[key] = value;
+  }else{
+    delete outPara[key];
+  }
+  parameters = [];
+  for(var k in outPara){
+    parameters.push(k + '=' + outPara[k]);
+  }
+
+  var finalUrl = baseUrl;
+
+  if(parameters.length>0){
+    finalUrl += '?' + parameters.join('&');
+  }
+
+  return finalUrl + url.substring(hashIndex);
+
+}
