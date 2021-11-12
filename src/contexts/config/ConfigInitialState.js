f9101075b815292a6e9164c0a5a61fd921c492ea
Masoud/ <masoud_tesna@live.com>
2021-11-12 04:41:56 +0330

    fix initial language state: change il8n() language for app...

--- src/contexts/config/ConfigInitialState.js
+++ src/contexts/config/ConfigInitialState.js
@@ -1,7 +1,7 @@
 // Initial State:
+import i18n from "../../translations";
 
-
-// get language from url or local storage in client browser:
+// get language from url & local storage in client browser:
 const getLangFromLocalStorage = window.localStorage.getItem('lang_code');
 const url = new URL(window.location.href);
 const getLangFromUrlParam = url.searchParams.get('lang_code');
@@ -11,19 +11,22 @@ const langCode = getLangFromUrlParam || getLangFromLocalStorage || 'en';
 
 const clientCurrencyLocalStorage = window.localStorage.getItem('client_currency');
 
-// if lang_code in url and url Lang code !== lang_code local storage => chang local storage lang_code:
-if (getLangFromUrlParam && getLangFromLocalStorage !== getLangFromUrlParam) {
+i18n
+  .changeLanguage(langCode)
+  .then(() => {
 
-  window.localStorage.setItem("lang_code", getLangFromUrlParam);
+    // if lang_code in url and url Lang code !== lang_code local storage => chang local storage lang_code:
+    (getLangFromUrlParam && getLangFromLocalStorage !== getLangFromUrlParam) && window.localStorage.setItem("lang_code", getLangFromUrlParam);
 
-}
+  })
+  .then(() => {
 
-// if not lang code in local storage set lang_code:
-if (!getLangFromLocalStorage) {
+    // if not lang code in local storage set lang_code:
+    !window.localStorage.getItem('lang_code') && window.localStorage.setItem("lang_code", 'en');
 
-  window.localStorage.setItem("lang_code", 'en');
+  });
 
-}
+
 
 
 if (!clientCurrencyLocalStorage) {
