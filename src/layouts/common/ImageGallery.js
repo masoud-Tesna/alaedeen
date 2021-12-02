643cef5301c721a6bd218a28d35c64d31bd6268f
/Masoud <masoud_tesna@live.com>
2021-12-03 02:01:08 +0330

    image gallery mobile level1 done...

--- src/layouts/common/ImageGallery.js
+++ src/layouts/common/ImageGallery.js
@@ -7,7 +7,7 @@ import './styles/ImageGallery.less';
 import { Swiper, SwiperSlide } from 'swiper/react/swiper-react';
 
 // import ant design components:
-import { Image } from 'antd';
+import { Col, Image, Row } from 'antd';
 
 // Import Swiper styles
 import "swiper/swiper.less";
@@ -16,16 +16,77 @@ import "swiper/modules/thumbs/thumbs.less";
 
 // import Swiper core and required modules
 import SwiperCore, { FreeMode, Thumbs } from 'swiper';
+import ShowResponsiveImage from "./ShowResponsiveImage";
 
 // install Swiper modules
 SwiperCore.use([FreeMode, Thumbs]);
 
-const ImageGallery = () => {
+const ImageGallery = ({ mainPair, imagePairs }) => {
+
+  if (Object.values(imagePairs).length) {
+    imagePairs[0] = mainPair;
+  }
+  //console.log(Object.values(imagePairs))
 
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
   return (
-    <h1>images</h1>
+    <Row className="imageGallery--container">
+      <Col span={24}>
+        <Image.PreviewGroup>
+          <Swiper
+            style={{
+              '--swiper-navigation-color': '#fff',
+              '--swiper-pagination-color': '#fff',
+            }}
+            spaceBetween={5}
+            navigation={false}
+            thumbs={{ swiper: thumbsSwiper }}
+            className="imageGallery--swiper"
+          >
+            {Object.values(imagePairs)?.map(image => {
+              return (
+                <SwiperSlide key={image?.pair_id}>
+                  <ShowResponsiveImage
+                    imagePath={ image?.detailed?.image_path }
+                    imageFolder='detailed'
+                    width={350}
+                    height={350}
+                    imageAlt={ image?.detailed?.alt }
+                    object_id={image?.pair_id}
+                    object_type={`prd`}
+                    preview
+                  />
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+        </Image.PreviewGroup>
+        <Swiper
+          onSwiper={setThumbsSwiper}
+          spaceBetween={5}
+          slidesPerView={5}
+          freeMode={true}
+          watchSlidesProgress={true}
+          className="imageGallery--thumbnailsSwiper">
+          {Object.values(imagePairs)?.map(image => {
+            return (
+              <SwiperSlide key={image?.pair_id}>
+                <ShowResponsiveImage
+                  imagePath={ image?.detailed?.image_path }
+                  imageFolder='detailed'
+                  width={68}
+                  height={68}
+                  imageAlt={ image?.detailed?.alt }
+                  object_id={`thumbs_${image?.pair_id}`}
+                  object_type={`prd`}
+                />
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </Col>
+    </Row>
   );
 };
 
