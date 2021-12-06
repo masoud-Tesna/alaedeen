cc8d2bf46634015dc48fdeb9149ee41094981e1e
/Masoud <masoud_tesna@live.com>
2021-12-06 18:56:30 +0330

    remove footer for product page...

--- src/templates/footer/index.js
+++ src/templates/footer/index.js
@@ -8,7 +8,7 @@ const SiteFooter = (props) => {
   const pathName = useParsPathName();
 
   // Check pathName For Set Default Top Panel Or Not:
-  if (pathName === 'sign-in' || pathName === 'all-categories' || pathName === 'categories' || pathName === 'register' || pathName === 'recommended') {
+  if (pathName === 'sign-in' || pathName === 'all-categories' || pathName === 'categories' || pathName === 'product' || pathName === 'register' || pathName === 'recommended') {
     return <></> // remove Footer
   }
 
