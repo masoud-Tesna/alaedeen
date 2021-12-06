603bda4ff8dde8e6c15073ec7d022b33120d2ee0
/Masoud <masoud_tesna@live.com>
2021-12-06 12:46:18 +0330

    add product feature type...

---
+++ src/templates/blocks/product_templates/components/FeatureTypeConst.js
@@ -0,0 +1,27 @@
+// Feature type:
+export const PrdFeatures_GROUP = "G";
+
+export const PrdFeatures_SINGLE_CHECKBOX = "C";
+export const PrdFeatures_MULTIPLE_CHECKBOX = "M";
+
+export const PrdFeatures_TEXT_SELECTBOX = "S";
+export const PrdFeatures_NUMBER_SELECTBOX = "N";
+
+export const PrdFeatures_EXTENDED = "E";
+
+export const PrdFeatures_TEXT_FIELD = "T";
+export const PrdFeatures_NUMBER_FIELD = "O";
+
+export const PrdFeatures_DATE = "D";
+
+export const PrdFeatures_SELECTABLE = [ PrdFeatures_TEXT_SELECTBOX, PrdFeatures_MULTIPLE_CHECKBOX, PrdFeatures_NUMBER_SELECTBOX, PrdFeatures_EXTENDED ];
+
+
+
+
+
+
+
+
+
+
