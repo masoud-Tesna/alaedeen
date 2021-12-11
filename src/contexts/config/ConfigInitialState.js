2a1a7ec6a624ad355d21c6500283c6174fd4aa59
/Masoud <masoud_tesna@live.com>
2021-12-12 02:34:38 +0330

    FIX: site default language set to Persian...

--- src/contexts/config/ConfigInitialState.js
+++ src/contexts/config/ConfigInitialState.js
@@ -1,12 +1,13 @@
 // Initial State:
 import i18n from "../../translations";
 
+const url = new URL(window.location.href);
+
 // get language from url & local storage in client browser:
 const getLangFromLocalStorage = window.localStorage.getItem('lang_code');
-const url = new URL(window.location.href);
 const getLangFromUrlParam = url.searchParams.get('lang_code');
 
-// if language in url isset, in localstorage isset, else isset 'en
+// if language in url isset, OR in localstorage isset, OR isset 'fa'
 const langCode = getLangFromUrlParam || getLangFromLocalStorage || 'fa';
 
 const clientCurrencyLocalStorage = window.localStorage.getItem('client_currency');
@@ -15,19 +16,16 @@ i18n
   .changeLanguage(langCode)
   .then(() => {
 
-    // if lang_code in url and url Lang code !== lang_code local storage => chang local storage lang_code:
+    // if lang_code in url and url lang_code !== lang_code local storage => chang local storage lang_code:
     (getLangFromUrlParam && getLangFromLocalStorage !== getLangFromUrlParam) && window.localStorage.setItem("lang_code", getLangFromUrlParam);
 
   })
   .then(() => {
 
-    // if not lang code in local storage set lang_code:
-    !window.localStorage.getItem('lang_code') && window.localStorage.setItem("lang_code", 'en');
+    // if not lang_code in local storage set lang_code:
+    !window.localStorage.getItem('lang_code') && window.localStorage.setItem("lang_code", 'fa');
 
   });
-
-
-
 
 if (!clientCurrencyLocalStorage) {
   window.localStorage.setItem("client_currency", "USD");
