5c3dd63b03c84c54d5cfea444b5998742b9e731f
/Masoud <masoud_tesna@live.com>
2021-12-21 11:36:31 +0330

    page component done...

--- src/templates/views/Page.js
+++ src/templates/views/Page.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 // import style file:
@@ -8,11 +9,16 @@ import { Col, Row, Skeleton } from "antd";
 import { useGetApi } from "../../functions";
 import { SeoGenerator } from "../../functions/Helper";
 
+
 const Page = () => {
   let { page: pageSeoName } = useParams();
 
   const { isLoading, data } = useGetApi(`page-api`, `page_seo=${pageSeoName}`, `storePage_${pageSeoName}`);
   const page = data || [];
+
+  useEffect(() => {
+    window.scroll({ top: 0, behavior: 'smooth' });
+  }, [pageSeoName]);
 
   return (
     <Row className={`page--container`}>
@@ -23,14 +29,14 @@ const Page = () => {
         canonical={ `https://alaedeen.com/page/${page?.seo_name}` }
       />
 
-      <Col span={24}>
+      <Col span={24} className="bg-white" style={{ padding: '2rem' }}>
         {isLoading &&
           <Skeleton  paragraph={{ rows: 25 }}  active />
         }
         {page?.page &&
           <>
             <Row>
-              <Col span={24} className={`page--name text-center`}>
+              <Col span={24} className={`page--name`}>
                 {page.page}
               </Col>
               <Col className="page--content" dangerouslySetInnerHTML={ {__html: page.description} } />
