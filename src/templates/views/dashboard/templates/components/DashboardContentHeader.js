66b42cf8e96216a33ed19eeddca323ab2645174c
/Masoud <masoud_tesna@live.com>
2022-01-04 12:50:08 +0330

    create dashboard content header..

--- src/templates/views/dashboard/templates/components/DashboardContentHeader.js
+++ src/templates/views/dashboard/templates/components/DashboardContentHeader.js
@@ -1,19 +1,30 @@
 import "./styles/DashboardContentHeader.less"
 
 import { Button, Col, Row } from "antd";
+import { useTranslation } from "react-i18next";
+import { __ } from "../../../../../functions/Helper";
 
-const DashboardContentHeader = ({page, dashboardToggleDrawer}) => {
+import {DashboardDrawerContext} from "../../templates/DashboardMain";
+
+const DashboardContentHeader = ({page}) => {
+
+  const { t } = useTranslation();
 
   return (
     <Row className="dashboardContentHeader" justify="space-between">
       <Col className="dashboardContentHeader--caption">
-        {page}
+        {t(__(page))}
       </Col>
 
       <Col className="d-lg-none dashboardContentHeader--toggleDrawer">
-        <Button type="link"  onClick={() => dashboardToggleDrawer()}>
-          <i className="fas fa-grip-lines" />
-        </Button>
+        <DashboardDrawerContext.Consumer>
+          {toggleDrawer => (
+            <Button type="link"  onClick={() => toggleDrawer()}>
+              <i className="fas fa-grip-lines" />
+            </Button>
+          )}
+        </DashboardDrawerContext.Consumer>
+
       </Col>
     </Row>
   );
