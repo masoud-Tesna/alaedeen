0a2d3197b880ae071dfd22d8c0cccca3b37d8953
/Masoud <masoud_tesna@live.com>
2022-01-04 05:36:36 +0330

    create dashboard route group and use in App.js file...

---
+++ src/templates/views/DashboardRoutes.js
@@ -0,0 +1,15 @@
+import { Route } from "react-router-dom";
+import { asyncComponent } from "../../functions/Helper";
+import DashboardMain from "./dashboard/templates/DashboardMain";
+
+const Dashboard = asyncComponent(() =>
+  import('./dashboard/pages/Dashboard').then(module => module.default)
+);
+
+const DashboardRoutes = () => ([
+  <Route exact path="/dashboard" element={<Dashboard />} />,
+  <Route exact path="/dashboard/account/password-reset" element={<DashboardMain> password-reset </DashboardMain>} />,
+  <Route exact path="/dashboard/account/manufacturer-information" element={<DashboardMain> manufacturer-information </DashboardMain>} />,
+]);
+
+export default DashboardRoutes;
