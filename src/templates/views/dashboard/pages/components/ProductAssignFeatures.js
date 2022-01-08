54ff984c2326f8bdcc101ab4307cf4cbee72ef26
/Masoud <masoud_tesna@live.com>
2022-01-08 17:32:14 +0330

    create ProductAssignFeatures Component for handle features lists...

--- src/templates/views/dashboard/pages/components/ProductAssignFeatures.js
+++ src/templates/views/dashboard/pages/components/ProductAssignFeatures.js
@@ -1,6 +1,7 @@
 import { __ } from "../../../../../functions/Helper";
 import { Form, Input } from "antd";
 import { useTranslation } from "react-i18next";
+import { useGetApi } from "../../../../../functions";
 
 const ProductAssignFeatures = ({category_id}) => {
 
@@ -8,21 +9,31 @@ const ProductAssignFeatures = ({category_id}) => {
 
   const { t } = useTranslation();
 
+  // get Feature list from API:
+  /*const { isLoading: featuresIsLoading, data: featuresData } = useGetApi(`feature-list-api`, `category_id=${category_id}`, `features_${category_id}`, { enabled: !!category_id });*/
+
+  const { isLoading: featuresIsLoading, data: featuresData } = useGetApi(`feature-list-api`, `category_id=942`, `features_${category_id}`);
+  const { features } = featuresData || [];
+
+  console.log(features)
+
   return (
-    <>
-      <Form.Item
-        name="page_title"
-        label={t('page_title')}
-        labelCol={{sm: 24, lg: 4}}
-        tooltip={t(__('page title tooltip message'))}
-      >
-        <TextArea
-          showCount
-          maxLength={60}
-          autoSize
-        />
-      </Form.Item>
-    </>
+    featuresIsLoading ?
+      <>IsLoading...</> :
+      <>
+        <Form.Item
+          name="page_title"
+          label={t('page_title')}
+          labelCol={{sm: 24, lg: 4}}
+          tooltip={t(__('page title tooltip message'))}
+        >
+          <TextArea
+            showCount
+            maxLength={60}
+            autoSize
+          />
+        </Form.Item>
+      </>
   );
 };
 
