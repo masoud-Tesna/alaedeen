a3b65ae5050970b7d76a2fe02edbc0a50bdc459f
/Masoud <masoud_tesna@live.com>
2022-01-12 07:45:43 +0330

    create spinner context...

---
+++ src/contexts/spiner/SpinnerContext.js
@@ -0,0 +1,63 @@
+import { createContext, useContext, useReducer } from "react";
+import LoaderSpinner from "../../templates/common/LoadSpinner";
+import { LOADING_TRUE } from "../config/ConfigActions";
+
+// spinner Context Create:
+const spinnerContext = createContext();
+
+// initial state:
+const spinnerInitialState = {
+  isLoading: false
+}
+
+// reducer function:
+function spinnerReducer(state, action) {
+  switch (action.type) {
+    case "loading_state" :
+      return {
+        ...state, isLoading: action.payload
+      };
+    default:
+      return state;
+  }
+}
+
+// create spinner provider:
+export const SpinnerProvider = ({ children }) => {
+
+  // useReducer For Language use in app
+  const [spinner, spinnerDispatch] = useReducer(
+    spinnerReducer,
+    spinnerInitialState
+  );
+
+  return (
+    <spinnerContext.Provider value={{ spinner, spinnerDispatch }} >
+      {spinner.isLoading &&
+        <LoaderSpinner spinner={'default'} spinnerColor={'#2e8339'}/>
+      }
+
+      {children}
+    </spinnerContext.Provider>
+  );
+
+}
+
+// get current spinner state
+export function useGetSpinner() {
+  const spinner = useContext(spinnerContext).spinner;
+  return { spinner };
+}
+
+//get spinner dispatch function:
+export function useSpinnerDispatch() {
+  const spinnerDispatch = useContext(spinnerContext).spinnerDispatch
+  return { spinnerDispatch };
+}
+
+export const isLoadingAction = val => {
+  return {
+    type: "loading_state",
+    payload: val
+  };
+}
