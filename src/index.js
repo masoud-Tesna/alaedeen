9f487c03762f8e31d5dd42f6b9caafa70723a9ef
/Masoud <masoud_tesna@live.com>
2022-02-28 10:50:57 +0330

    install sentry and use.. (for show error)

--- src/index.js
+++ src/index.js
@@ -23,12 +23,24 @@ import { ConfigProvider } from "./contexts/config/ConfigContext";
 // import user  context:
 import { UserProvider } from "./contexts/user/UserContext";
 
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
 // import Application:
 import App from './App';
 
 // initial query client:
 const queryClient = new QueryClient();
 
+Sentry.init({
+  dsn: "https://fa9d9d9d6d4448e4a01fd5b98d5520f1@o1154697.ingest.sentry.io/6234525",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 const Application = () => {
   return (
