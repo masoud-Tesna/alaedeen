87760fd000d11aea3d04a5539bd29a7ed3c5cff9
/Masoud <masoud_tesna@live.com>
2022-03-14 02:43:48 +0330

    add isLoading Skeleton for plan pricing section...

--- src/templates/views/dashboard/pages/plans/index.js
+++ src/templates/views/dashboard/pages/plans/index.js
@@ -1,6 +1,6 @@
 import "./styles/Plans.less";
 
-import { Button, Col, Empty, Row, Statistic } from "antd";
+import { Button, Col, Empty, Row, Skeleton, Statistic } from "antd";
 import DashboardContentHeader from "../../templates/components/DashboardContentHeader";
 import Pricing from "../../../../common/Pricing";
 import { __, fn_after_discount, fn_discount, SeoGenerator } from "../../../../../functions/Helper";
@@ -136,201 +136,236 @@ const Plans = () => {
       </Col>
 
       <Col span={24} className="plans--container">
-        <Row gutter={[20, 20]} justify="center">
 
-          <Col span={24} className="DiscountDetails">
-            <Row gutter={[0, 20]}>
-              <Col span={24} className={ `__base ${hasAffiliateDiscount ? 'plusAffiliate': ''}` }>
-                {t('base_prise_discount_msg')}
-              </Col>
-              { hasAffiliateDiscount &&
-              <Col span={ 24 } className="__affiliate">
-                { t('affiliate_discount_msg') }
-              </Col>
-              }
-            </Row>
-          </Col>
-
-          { (isLoading || !plans?.length || user_data?.load) ?
-            <>Loading...</> :
-            plans?.map(plan => {
-              return (
-                <Col key={`plansList_${+(plan?.plan_id)}`} xs={24} md={8}>
-                  <Pricing
-                    planId               = { +(plan?.plan_id) }
-                    plan                 = { plan?.plan }
-                    planType             = { plan?.type }
-                    productsLimit        = { +(plan?.products_limit) }
-                    hasAffiliateDiscount = { hasAffiliateDiscount }
-                    planDiscount         = { planDiscount }
-                    baseDiscount         = { plan?.base_discount }
-                    afterDiscount        = { plan?.after_discount }
-                    price                = { +(plan?.price) }
-                    priceText            = { plan?.price_text }
-                    basePrice            = { plan?.base_price }
-                    color                = { plan?.color }
-                    gradient             = { plan?.gradient }
-                    backgroundColor      = { plan?.background }
-                    features             = { plan?.features || {} }
-                    planIds              = { planIds }
-                    handlePriceList      = { handlePriceList }
-                  />
+        { (isLoading || !plans?.length || user_data?.load) ?
+          <Row gutter={[0, 20]} justify="center">
+            <Col span={24} className="DiscountDetails">
+              <Row gutter={[0, 15]}>
+                <Col span={24}>
+                  <Skeleton.Input style={{ width: "calc(.32 * 100vw - 100px)", height: 16 }} active={true} />
                 </Col>
-              )
-            })
-          }
-        </Row>
 
-        <Row gutter={[0, 20]} className="chosen--plans">
-          <Col span={24} className="__caption">{t("your_chosen_plan")}:</Col>
-          {(planIds?.length && plans.length) ?
-            <>
-              <Col span={24} className="chosen--plans__table">
-                <div>
-                  <Row className="__head">
-                    <Col className="text-center my-auto" span={8}>
-                      {t('plan')}
+                <Col span={24}>
+                  <Skeleton.Input style={{ width: "calc(.37 * 100vw - 100px)", height: 16 }} active={true} />
+                </Col>
+              </Row>
+            </Col>
+
+            <Col span={24}>
+              <Row gutter={[20, 20]} className="isLoading--Pricing" justify="center">
+                {new Array(4).fill("", 0, 4).map((__, i) => {
+                  return(
+                    <Col xs={24} md={8} key={`isLoading_prising_${i + 1}`}>
+                      <Skeleton.Input style={{ height: "45rem" }} active={true} />
                     </Col>
-                    <Col className="text-center my-auto" span={4}>
-                      {t('price')}
+                  )
+                })}
+              </Row>
+            </Col>
+
+            <Col span={24}>
+              <Skeleton paragraph={{ rows: 5 }} active={true} />
+            </Col>
+          </Row> :
+          <>
+            <Row gutter={[20, 20]} justify="center">
+
+              <Col span={24} className="DiscountDetails">
+                <Row gutter={[0, 20]}>
+                  <Col span={24} className={ `__base ${hasAffiliateDiscount ? 'plusAffiliate': ''}` }>
+                    {t('base_prise_discount_msg')}
+                  </Col>
+                  { hasAffiliateDiscount &&
+                  <Col span={ 24 } className="__affiliate">
+                    { t('affiliate_discount_msg') }
+                  </Col>
+                  }
+                </Row>
+              </Col>
+
+              {
+                plans?.map(plan => {
+                  return (
+                    <Col key={`plansList_${+(plan?.plan_id)}`} xs={24} md={8}>
+                      <Pricing
+                        planId               = { +(plan?.plan_id) }
+                        plan                 = { plan?.plan }
+                        planType             = { plan?.type }
+                        productsLimit        = { +(plan?.products_limit) }
+                        hasAffiliateDiscount = { hasAffiliateDiscount }
+                        planDiscount         = { planDiscount }
+                        baseDiscount         = { plan?.base_discount }
+                        afterDiscount        = { plan?.after_discount }
+                        price                = { +(plan?.price) }
+                        priceText            = { plan?.price_text }
+                        basePrice            = { plan?.base_price }
+                        color                = { plan?.color }
+                        gradient             = { plan?.gradient }
+                        backgroundColor      = { plan?.background }
+                        features             = { plan?.features || {} }
+                        planIds              = { planIds }
+                        handlePriceList      = { handlePriceList }
+                      />
                     </Col>
-                    <Col className="text-center my-auto" span={4}>
-                      {t('discount')}
-                    </Col>
-                    <Col className="text-center my-auto" span={4}>
-                      {t('discount_price')}
-                    </Col>
-                    <Col className="text-center my-auto" span={4}>
-                      {t('amount_including_discount')}
-                    </Col>
-                  </Row>
+                  )
+                })
+              }
 
-                  <Row className="__body">
-                    {planIds?.map(planId => {
-                      const plan = plans?.find(plan => +(plan?.plan_id) === planId)?.plan;
-                      const basePrice = plans?.find(plan => +(plan?.plan_id) === planId)?.base_price;
-                      const baseDiscount = plans?.find(plan => +(plan?.plan_id) === planId)?.base_discount;
-                      const productsLimit = plans?.find(plan => +(plan?.plan_id) === planId)?.products_limit;
-                      const planType = plans?.find(plan => +(plan?.plan_id) === planId)?.type;
-                      const price = plans?.find(plan => +(plan?.plan_id) === planId)?.price;
+            </Row>
 
-                      let priceBeforeDiscount = 0,
-                        priceAfterDiscount = 0,
-                        allDiscount = +baseDiscount || 0;
+            <Row gutter={[0, 20]} className="chosen--plans">
+              <Col span={24} className="__caption">{t("your_chosen_plan")}:</Col>
+              {(planIds?.length && plans.length) ?
+                <>
+                  <Col span={24} className="chosen--plans__table">
+                    <div>
+                      <Row className="__head">
+                        <Col className="text-center my-auto" span={8}>
+                          {t('plan')}
+                        </Col>
+                        <Col className="text-center my-auto" span={4}>
+                          {t('price')}
+                        </Col>
+                        <Col className="text-center my-auto" span={4}>
+                          {t('discount')}
+                        </Col>
+                        <Col className="text-center my-auto" span={4}>
+                          {t('discount_price')}
+                        </Col>
+                        <Col className="text-center my-auto" span={4}>
+                          {t('amount_including_discount')}
+                        </Col>
+                      </Row>
 
-                      if (planType === "P") { // if plan is product :
-                        priceAfterDiscount = +productsLimit * fn_discount(basePrice, allDiscount);
-                        priceBeforeDiscount = +productsLimit * basePrice;
-                      }
-                      else if (planType === "S") { // if plan is personal store:
-                        priceAfterDiscount = fn_discount(+price, allDiscount);
-                        priceBeforeDiscount = +price;
-                      }
+                      <Row className="__body">
+                        {planIds?.map(planId => {
+                          const plan = plans?.find(plan => +(plan?.plan_id) === planId)?.plan;
+                          const basePrice = plans?.find(plan => +(plan?.plan_id) === planId)?.base_price;
+                          const baseDiscount = plans?.find(plan => +(plan?.plan_id) === planId)?.base_discount;
+                          const productsLimit = plans?.find(plan => +(plan?.plan_id) === planId)?.products_limit;
+                          const planType = plans?.find(plan => +(plan?.plan_id) === planId)?.type;
+                          const price = plans?.find(plan => +(plan?.plan_id) === planId)?.price;
 
-                      return (
-                        <Col span={24} key={`chosenPlan_${planId}`}>
-                          <Row className="__data">
-                            <Col span={8} className="text-center my-auto __plan">{ plan }</Col>
+                          let priceBeforeDiscount = 0,
+                            priceAfterDiscount = 0,
+                            allDiscount = +baseDiscount || 0;
 
-                            <Col span={4} className="text-center my-auto __price">
-                              <Statistic value={priceBeforeDiscount} />
+                          if (planType === "P") { // if plan is product :
+                            priceAfterDiscount = +productsLimit * fn_discount(basePrice, allDiscount);
+                            priceBeforeDiscount = +productsLimit * basePrice;
+                          }
+                          else if (planType === "S") { // if plan is personal store:
+                            priceAfterDiscount = fn_discount(+price, allDiscount);
+                            priceBeforeDiscount = +price;
+                          }
+
+                          return (
+                            <Col span={24} key={`chosenPlan_${planId}`}>
+                              <Row className="__data">
+                                <Col span={8} className="text-center my-auto __plan">{ plan }</Col>
+
+                                <Col span={4} className="text-center my-auto __price">
+                                  <Statistic value={priceBeforeDiscount} />
+                                </Col>
+
+                                <Col span={4} className="text-center my-auto __price">
+                                  { allDiscount ?
+                                    <Statistic
+                                      value={ allDiscount } prefix={ config?.language === "en" ? "%" : "" }
+                                      suffix={ config?.language !== "en" ? "%" : "" }
+                                    /> :
+                                    allDiscount
+                                  }
+                                </Col>
+
+                                <Col span={4} className="text-center my-auto __price">
+                                  <Statistic value={fn_after_discount(priceBeforeDiscount, allDiscount)} />
+                                </Col>
+
+                                <Col span={4} className="text-center my-auto __price">
+                                  <Statistic value={priceAfterDiscount} />
+                                </Col>
+                              </Row>
+                            </Col>
+                          )
+                        })}
+                      </Row>
+                    </div>
+                  </Col>
+
+                  <Col span={24} className="chosen--plans__table mt-5">
+                    <div>
+                      <Row className="__body">
+                        <Col span={24}>
+                          <Row className="__data" justify="space-between">
+                            <Col className="my-auto __tax">
+                              {t('total_price')} :
                             </Col>
 
-                            <Col span={4} className="text-center my-auto __price">
-                              { allDiscount ?
-                                <Statistic
-                                  value={ allDiscount } prefix={ config?.language === "en" ? "%" : "" }
-                                  suffix={ config?.language !== "en" ? "%" : "" }
-                                /> :
-                                allDiscount
-                              }
-                            </Col>
-
-                            <Col span={4} className="text-center my-auto __price">
-                              <Statistic value={fn_after_discount(priceBeforeDiscount, allDiscount)} />
-                            </Col>
-
-                            <Col span={4} className="text-center my-auto __price">
-                              <Statistic value={priceAfterDiscount} />
+                            <Col className="my-auto __taxPrice">
+                              <Statistic value={priceList} />
                             </Col>
                           </Row>
                         </Col>
-                      )
-                    })}
-                  </Row>
-                </div>
-              </Col>
 
-              <Col span={24} className="chosen--plans__table mt-5">
-                <div>
-                  <Row className="__body">
-                    <Col span={24}>
-                      <Row className="__data" justify="space-between">
-                        <Col className="my-auto __tax">
-                          {t('total_price')} :
+                        {hasAffiliateDiscount &&
+                        <Col span={24}>
+                          <Row className="__data" justify="space-between">
+                            <Col className="my-auto __tax">
+                              { config?.language === "en" ?
+                                `${ t('discount') } (%${ planDiscount }) :` :
+                                `${ t('discount') } (${ planDiscount }%) :`
+                              }
+                            </Col>
+
+                            <Col className="my-auto __taxPrice">
+                              <Statistic value={totalPrice} />
+                            </Col>
+                          </Row>
+                        </Col>
+                        }
+
+                        <Col span={24}>
+                          <Row className="__data" justify="space-between">
+                            <Col className="my-auto __tax">
+                              { config?.language === "en" ?
+                                `${ t('tax') } (%9) :` :
+                                `${ t('tax') } (9%) :`
+                              }
+                            </Col>
+
+                            <Col className="my-auto __taxPrice">
+                              <Statistic value={tax} />
+                            </Col>
+                          </Row>
                         </Col>
 
-                        <Col className="my-auto __taxPrice">
-                          <Statistic value={priceList} />
-                        </Col>
-                      </Row>
-                    </Col>
+                        <Col span={24}>
+                          <Row className="__data" justify="space-between">
+                            <Col className="my-auto __tax">
+                              {t('the_amount_payable')} :
+                            </Col>
 
-                    {hasAffiliateDiscount &&
-                      <Col span={24}>
-                        <Row className="__data" justify="space-between">
-                          <Col className="my-auto __tax">
-                            { config?.language === "en" ?
-                              `${ t('discount') } (%${ planDiscount }) :` :
-                              `${ t('discount') } (${ planDiscount }%) :`
-                            }
-                          </Col>
-
-                          <Col className="my-auto __taxPrice">
-                            <Statistic value={totalPrice} />
-                          </Col>
-                        </Row>
-                      </Col>
-                    }
-
-                    <Col span={24}>
-                      <Row className="__data" justify="space-between">
-                        <Col className="my-auto __tax">
-                          { config?.language === "en" ?
-                            `${ t('tax') } (%9) :` :
-                            `${ t('tax') } (9%) :`
-                          }
-                        </Col>
-
-                        <Col className="my-auto __taxPrice">
-                          <Statistic value={tax} />
+                            <Col className="my-auto __taxPrice">
+                              <Statistic value={totalPrice + tax} />
+                            </Col>
+                          </Row>
                         </Col>
                       </Row>
-                    </Col>
+                    </div>
+                  </Col>
 
-                    <Col span={24}>
-                      <Row className="__data" justify="space-between">
-                        <Col className="my-auto __tax">
-                          {t('the_amount_payable')} :
-                        </Col>
+                  <Col span={24} className="text-center mt-4">
+                    <Button className="bg-primary text-white border-0 __payBtn" htmlType="submit" onClick={handlePay}>{t(__('payment'))}</Button>
+                  </Col>
+                </> :
 
-                        <Col className="my-auto __taxPrice">
-                          <Statistic value={totalPrice + tax} />
-                        </Col>
-                      </Row>
-                    </Col>
-                  </Row>
-                </div>
-              </Col>
+                <Col span={24} className="text-center"><Empty description={t("no_selected_plan")}/></Col>
+              }
+            </Row>
+          </>
+        }
 
-              <Col span={24} className="text-center mt-4">
-                <Button className="bg-primary text-white border-0 __payBtn" htmlType="submit" onClick={handlePay}>{t(__('payment'))}</Button>
-              </Col>
-            </> :
-
-            <Col span={24} className="text-center"><Empty description={t("no_selected_plan")}/></Col>
-          }
-        </Row>
       </Col>
     </Row>
   );
