b26a2f33277a19979a3d90296c60524a7a1e3b33
/Masoud <masoud_tesna@live.com>
2022-03-14 18:52:21 +0330

    user details section move in to menu side...

--- src/templates/views/dashboard/templates/components/DashboardSidenav.js
+++ src/templates/views/dashboard/templates/components/DashboardSidenav.js
@@ -163,7 +163,7 @@ const DashboardSidenav = ({ dashboardToggleDrawer }) => {
   }
 
   return (
-    <Row gutter={[0, 20]} className="dashboard--side__container">
+    <Row className="dashboard--side__container">
       <Col span={24} className="my-auto alaedeen--logo">
         <div className="logo">
           <Link to={"/"} className="d-block">
@@ -183,107 +183,111 @@ const DashboardSidenav = ({ dashboardToggleDrawer }) => {
         </div>
       </Col>
 
-      <Col span={24} className="user--details">
-        <Row gutter={10}>
-          <Col flex="55px" className="--avatar">
-            { (user_data?.auth?.company_logo && user_data?.auth?.company_logo.length !== 0) ?
-              <ShowResponsiveImage
-                imagePath={ user_data?.auth?.company_logo?.logo_path }
-                imageFolder='company_logo'
-                width={55}
-                height={55}
-                skeletonWidth="55px"
-                skeletonHeight="55px"
-                skeletonRadius="55%"
-                skeletonSvgWidth="30px"
-                imageAlt={ user_data?.auth?.company ? user_data?.auth?.company : ` ${user_data?.auth?.firstname} ${user_data?.auth?.lastname} `}
-                object_id={user_data?.auth?.company_id}
-                object_type={`company_logo${config.language}`}
-              /> :
-              <i className="fal fa-user display-3 text-70 d-block" />
-            }
-          </Col>
-
-          <Col flex="1 1" className="--details">
-            { user_data?.auth?.company || `${user_data?.auth?.firstname} ${user_data?.auth?.lastname}` }
-          </Col>
-        </Row>
-      </Col>
-
       <Col span={24} className="dashboard--menu">
-        <Menu
-          mode="inline"
-          style={{ height: '100%', borderRight: 0 }}
-          className="side--menu"
-          onOpenChange={onOpenChange}
-          openKeys={openKeys}
-          defaultOpenKeys={openKeys}
-          selectedKeys={openKeys}
-        >
-          <Menu.Item key="dashboard"  icon={<HomeOutlined />}>
-            <Link to="/dashboard" className="side--link">
-              { t('dashboard') }
-            </Link>
-          </Menu.Item>
+        <Row gutter={[0, 20]}>
+          <Col span={24} className="user--details">
+            <Row gutter={10}>
+              <Col flex="55px" className="--avatar">
+                { (user_data?.auth?.company_logo && user_data?.auth?.company_logo.length !== 0) ?
+                  <ShowResponsiveImage
+                    imagePath={ user_data?.auth?.company_logo?.logo_path }
+                    imageFolder='company_logo'
+                    width={55}
+                    height={55}
+                    skeletonWidth="55px"
+                    skeletonHeight="55px"
+                    skeletonRadius="55%"
+                    skeletonSvgWidth="30px"
+                    imageAlt={ user_data?.auth?.company ? user_data?.auth?.company : ` ${user_data?.auth?.firstname} ${user_data?.auth?.lastname} `}
+                    object_id={user_data?.auth?.company_id}
+                    object_type={`company_logo${config.language}`}
+                  /> :
+                  <i className="fal fa-user display-3 text-70 d-block" />
+                }
+              </Col>
 
-          <SubMenu key="language" icon={<GlobalOutlined />} title={ t('language') }>
-            <Item onClick={() => handleChangeLanguage('fa')} key="1">فارسی</Item>
-            <Item onClick={() => handleChangeLanguage('en')} key="2">English</Item>
-            <Item onClick={() => handleChangeLanguage('ar')} key="3">عربی</Item>
-          </SubMenu>
+              <Col flex="1 1" className="--details">
+                { user_data?.auth?.company || `${user_data?.auth?.firstname} ${user_data?.auth?.lastname}` }
+              </Col>
+            </Row>
+          </Col>
 
-          <SubMenu key="account" icon={<i className="fal fa-user-cog " />} title={ t('account_settings') }>
-            <Item key="manufacturer-information">
-              <Link to="/dashboard/account/manufacturer-information" className="side--link">
-                { t('manufacturer_information') }
-              </Link>
-            </Item>
-            {/*<Item key="password-reset">
+          <Col span={24}>
+            <Menu
+              mode="inline"
+              style={{ height: '100%', borderRight: 0 }}
+              className="side--menu"
+              onOpenChange={onOpenChange}
+              openKeys={openKeys}
+              defaultOpenKeys={openKeys}
+              selectedKeys={openKeys}
+            >
+              <Menu.Item key="dashboard"  icon={<HomeOutlined />}>
+                <Link to="/dashboard" className="side--link">
+                  { t('dashboard') }
+                </Link>
+              </Menu.Item>
+
+              <SubMenu key="language" icon={<GlobalOutlined />} title={ t('language') }>
+                <Item onClick={() => handleChangeLanguage('fa')} key="1">فارسی</Item>
+                <Item onClick={() => handleChangeLanguage('en')} key="2">English</Item>
+                <Item onClick={() => handleChangeLanguage('ar')} key="3">عربی</Item>
+              </SubMenu>
+
+              <SubMenu key="account" icon={<i className="fal fa-user-cog " />} title={ t('account_settings') }>
+                <Item key="manufacturer-information">
+                  <Link to="/dashboard/account/manufacturer-information" className="side--link">
+                    { t('manufacturer_information') }
+                  </Link>
+                </Item>
+                {/*<Item key="password-reset">
               <Link to="/dashboard/account/password-reset" className="side--link">
                 { t('password_reset') }
               </Link>
             </Item>*/}
-          </SubMenu>
+              </SubMenu>
 
-          <Menu.Item key="support" icon={ <i className="fal fa-user-headset" /> }>
-            <a href="https://alaedeen.com/horn/my-tickets/" className="side--link">
-              { t('support') }
-            </a>
-          </Menu.Item>
+              <Menu.Item key="support" icon={ <i className="fal fa-user-headset" /> }>
+                <a href="https://alaedeen.com/horn/my-tickets/" className="side--link">
+                  { t('support') }
+                </a>
+              </Menu.Item>
 
-          <SubMenu key="requests" icon={ <i className="fa-light fa-comment-quote" /> } title={ t('requests') }>
+              <SubMenu key="requests" icon={ <i className="fa-light fa-comment-quote" /> } title={ t('requests') }>
 
-            {requestShowCondition(userType)}
+                {requestShowCondition(userType)}
 
-          </SubMenu>
+              </SubMenu>
 
-          <SubMenu key="products" icon={<i className="fab fa-product-hunt " />} title={ t('products_and_categories') }>
-            <Item key="manageProducts">
-              <Link to="/dashboard/products/manage" className="side--link">
-                { t('manage_products') }
-              </Link>
-            </Item>
-            <Item key="manageCategories">
-              <Link to="/dashboard/categories/manage" className="side--link">
-                { t('manage_categories') }
-              </Link>
-            </Item>
-          </SubMenu>
+              <SubMenu key="products" icon={<i className="fab fa-product-hunt " />} title={ t('products_and_categories') }>
+                <Item key="manageProducts">
+                  <Link to="/dashboard/products/manage" className="side--link">
+                    { t('manage_products') }
+                  </Link>
+                </Item>
+                <Item key="manageCategories">
+                  <Link to="/dashboard/categories/manage" className="side--link">
+                    { t('manage_categories') }
+                  </Link>
+                </Item>
+              </SubMenu>
 
-          <Menu.Item key="plans"  icon={ <i className="fal fa-box-check" /> }>
-            <Link to="/dashboard/plans" className="side--link">
-              { t('plans_and_pricing') }
-            </Link>
-          </Menu.Item>
+              <Menu.Item key="plans"  icon={ <i className="fal fa-box-check" /> }>
+                <Link to="/dashboard/plans" className="side--link">
+                  { t('plans_and_pricing') }
+                </Link>
+              </Menu.Item>
 
-          {planId === "14" &&
-          <Menu.Item key="affiliate"  icon={ <i className="fa-regular fa-users-medical" /> }>
-            <Link to="/dashboard/affiliate" className="side--link">
-              { t('affiliate') }
-            </Link>
-          </Menu.Item>
-          }
-        </Menu>
+              {planId === "14" &&
+              <Menu.Item key="affiliate"  icon={ <i className="fa-regular fa-users-medical" /> }>
+                <Link to="/dashboard/affiliate" className="side--link">
+                  { t('affiliate') }
+                </Link>
+              </Menu.Item>
+              }
+            </Menu>
+          </Col>
+        </Row>
       </Col>
     </Row>
   );
