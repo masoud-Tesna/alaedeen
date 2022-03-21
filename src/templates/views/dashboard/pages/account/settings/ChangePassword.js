f1d4ede3046931ecf9aa5001d100e006e40346d6
Masoud <masoud_tesna@live.com>
2022-03-21 05:54:30 +0330

    change password forms create...

--- src/templates/views/dashboard/pages/account/settings/ChangePassword.js
+++ src/templates/views/dashboard/pages/account/settings/ChangePassword.js
@@ -1,16 +1,22 @@
-import { Col, Form, Row } from "antd";
+import { Button, Col, Form, Input, Row } from "antd";
 import { isLoadingAction, useSpinnerDispatch } from "../../../../../../contexts/spiner/SpinnerContext";
 import { useTranslation } from "react-i18next";
 import axios from "axios";
+import { useGetAuthState } from "../../../../../../contexts/user/UserContext";
+import { __ } from "../../../../../../functions/Helper";
+import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
 
 const ChangePassword = () => {
 
-  const [ editProfileForm ] = Form.useForm();
+  const [ changePasswordForm ] = Form.useForm();
 
   // spinner dispatch context:
   const { spinnerDispatch } = useSpinnerDispatch();
 
   const { t } = useTranslation();
+
+  // user data context state:
+  const { user_data } = useGetAuthState();
 
   // create phone verify axios async function:
   async function changePasswordApi(userId, values) {
@@ -20,29 +26,123 @@ const ChangePassword = () => {
     );
   }
 
-  const editProfileOnFinish = values => {
+  const changePasswordOnFinish = values => {
 
-    values.user_type = userType;
+    values.operation = "change_password";
 
-    if (userType === "V") {
-      values.fields[65] = values?.fields[65].join(",");
-    }
+    console.log(values)
 
-    spinnerDispatch(isLoadingAction(true));
+    //spinnerDispatch(isLoadingAction(true));
 
-    UpdateProfileApi(+(user_data?.auth?.user_id), values)
+    /*changePasswordApi(+(user_data?.auth?.user_id), values)
       .then(res => {
         if (res?.data[0] === "update_done") spinnerDispatch(isLoadingAction(false));
-      });
+      });*/
 
   }
 
   return (
-    <Row>
-      <Col>
-        PasswordChange
-      </Col>
-    </Row>
+    <Form
+      className="h-100 settings--formContent"
+      name="editProfile-form"
+      form={changePasswordForm}
+      scrollToFirstError
+      onFinish={ changePasswordOnFinish }
+    >
+      <Row className="changePasswordForm" gutter={[0, 5]}>
+        <Col span={24}>
+          <Row>
+            <Col xs={24} md={12} lg={10} xl={9}>
+              <Form.Item
+                name="old_password"
+                label={ t(__('old_password')) }
+                labelCol={{sm: 24, lg: 7}}
+                rules={ [
+                  {
+                    required: true,
+                    message: t(__("Please input old password")),
+                  },
+                ] }
+              >
+                <Input.Password
+                  iconRender={ visible => (visible ? <EyeTwoTone/> : <EyeInvisibleOutlined/>) }
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+        </Col>
+
+        <Col span={24}>
+          <Row>
+            <Col xs={24} md={12} lg={10} xl={9}>
+              <Form.Item
+                name="password"
+                label={ t(__('new_password')) }
+                labelCol={{sm: 24, lg: 7}}
+                rules={[
+                  {
+                    required: true,
+                    message: t(__("Please input new password")),
+                  },
+                ]}
+              >
+                <Input.Password
+                  iconRender={ visible => (visible ? <EyeTwoTone/> : <EyeInvisibleOutlined/>) }
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+        </Col>
+
+        <Col span={24}>
+          <Row>
+            <Col xs={24} md={12} lg={10} xl={9}>
+              <Form.Item
+                name="confirm_password"
+                label={ t(__('confirm_password')) }
+                labelCol={{sm: 24, lg: 7}}
+                dependencies={['password']}
+                hasFeedback
+                rules={[
+                  {
+                    required: true,
+                    message: t(__("Please confirm new password")),
+                  },
+                  ({ getFieldValue }) => ({
+                    validator(_, value) {
+                      if (!value || getFieldValue('password') === value) {
+                        return Promise.resolve();
+                      }
+
+                      return Promise.reject(new Error(t(__('The two passwords that you entered do not match'))));
+                    },
+                  }),
+                ]}
+              >
+                <Input.Password
+                  iconRender={ visible => (visible ? <EyeTwoTone/> : <EyeInvisibleOutlined/>) }
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+        </Col>
+
+        <Col span={24}>
+          <Row>
+            <Col xs={24} md={12} lg={10} xl={9} className="mt-4 text-center">
+              <Form.Item
+                className="changePassword--btn"
+                valuePropName="checked"
+              >
+                <Button type="primary" htmlType="submit">
+                  { t(__('change_password')) }
+                </Button>
+              </Form.Item>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Form>
   );
 };
 
