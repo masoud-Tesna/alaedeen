d92b7e5c33699ea1e5d537c61c84775556049abd
Masoud <masoud_tesna@live.com>
2022-04-06 18:00:04 +0430

    use chat component...

--- src/templates/views/dashboard/pages/requests/public/Conversation.js
+++ src/templates/views/dashboard/pages/requests/public/Conversation.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import "../styles/Conversation.less";
-import { Button, Col, Empty, Form, Input, Row, Skeleton, Spin } from "antd";
-import { __, nl2br, SeoGenerator } from "../../../../../../functions/Helper";
+import { Col, Empty, Row, Skeleton } from "antd";
+import { SeoGenerator } from "../../../../../../functions/Helper";
 import DashboardContentHeader from "../../../templates/components/DashboardContentHeader";
 import { useGetApi } from "../../../../../../functions";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
@@ -9,28 +9,13 @@ import { useGetAuthState } from "../../../../../../contexts/user/UserContext";
 import { ArrowLeftOutlined } from "@ant-design/icons";
 import ShowResponsiveImage from "../../../../../common/ShowResponsiveImage";
 import { useTranslation } from "react-i18next";
-import axios from "axios";
-import { useGetConfig } from "../../../../../../contexts/config/ConfigContext";
-import Moment from "react-moment";
-import moment from "moment-jalaali";
-import fa from "moment/locale/fa";
+import Chat from "../components/Chat";
 
 const Conversation = () => {
-
-  const { TextArea } = Input;
-
-  // get initial config:
-  const { config } = useGetConfig();
 
   const navigate = useNavigate();
 
   const { t } = useTranslation();
-
-  // initial setting for Moment (Show date)
-  if (config.language !== 'en') {
-    moment.updateLocale("fa", fa);
-    moment.loadPersian({usePersianDigits: true});
-  }
 
   const { pathname } = useLocation();
 
@@ -46,6 +31,7 @@ const Conversation = () => {
 
   // state for save user id and type:
   const [user, setUser] = useState({});
+
 
   // for set user id and user type
   useEffect(() => {
@@ -81,25 +67,6 @@ const Conversation = () => {
 
   const [conversationData, setConversationData] = useState({});
 
-  // get Conversation Messages lists from API:
-  const { isLoading: messagesListIsLoading, isSuccess: messagesListIsSuccess, data: messagesListData, refetch: messagesListRefetch } = useGetApi(
-    `ConversationMessages/${conversationData?.id}`,
-    {},
-    `private_${requestMode}_conversation_messages_list_${conversationData?.id}`,
-    {
-      enabled: !!(conversationData?.conversation_id && user?.id),
-      refetchInterval: 10000
-    }
-  );
-
-  const messages = messagesListData?.messages || {},
-    messagesParams = messagesListData?.params || {};
-
-  const [messageText, setMessageText] = useState("");
-  const [isSpinSend, setIsSpinSend] = useState(false); // state for show spin after send message
-
-  const [sendMessageForm] = Form.useForm();
-
   // useEffect for change rayChat style after Mount And unMount Component.
   useEffect(() => {
     const interval = setInterval(() => {
@@ -115,39 +82,6 @@ const Conversation = () => {
       }
     };
   }, []);
-
-  // function for send message:
-  const handleSendMessage = (values) => {
-    setIsSpinSend(true); // show spin
-
-    values.conversation_id = conversationData?.conversation_id; // add conversation id
-    values.sender = user?.id; // add sender id
-    values.receiver = conversationData?.receiver_id; // add receiver id
-    values.category = "Message"; // add Message Category
-    values.type = "text"; // add Message type
-
-    const params = new URLSearchParams(values);
-
-    axios.post(
-      "https://alaedeen.com/horn/api/ConversationMessages",
-      params,
-      {
-        headers: {
-          "Content-Type": "text/plain"
-        }
-      }
-    )
-      .then(() => {
-        sendMessageForm.resetFields(); // reset send message form
-        messagesListRefetch() // re fetch message list
-          .then(() => {
-            setIsSpinSend(false); // remove spin
-          })
-          .then(() => {
-            messageScrollBottom(); // scroll bottom
-          });
-      });
-  }
 
   const messageContent = useRef(null); // use ref for messages list content
 
@@ -263,101 +197,13 @@ const Conversation = () => {
             {conversations.length ?
               (
                 conversationData?.conversation_id ?
-                  <Row>
-                    <Col span={24} className="--messages" ref={messageContent}>
-                      <Spin
-                        spinning={isSpinSend || messagesListIsLoading}
-                        tip={ isSpinSend ? `${ t('send_your_message') }...` :  `${ t('loading_conversation') }...`}
-                      >
-                        <Row gutter={[0, 25]}>
-                          {messages.length ?
-                            messages?.map(message => {
-                              const messageText = nl2br(message?.message);
-                              return (
-                                <Col
-                                  key={`message_${message?.message_id}`}
-                                  span={24}
-                                  className={`__message ${+(message?.sender?.user_id) === +(user?.id) ? 'send' : 'receive'}`}
-                                >
-                                  <Row gutter={10} className="messageBody">
-                                    <Col flex={ message?.sender?.avatar?.logo_path ? "55px" : "40px" } className="--avatar">
-                                      {
-                                        message?.sender?.avatar?.logo_path ?
-                                          <ShowResponsiveImage
-                                            imagePath={ message?.sender?.avatar?.logo_path }
-                                            imageFolder='company_logo'
-                                            width={60}
-                                            height={60}
-                                            skeletonWidth="60px"
-                                            skeletonHeight="60px"
-                                            skeletonRadius="60%"
-                                            skeletonSvgWidth="30px"
-                                            imageAlt={ message?.sender?.name }
-                                            object_id={ message?.sender?.user_id }
-                                            object_type={`receiver_logo${message?.sender?.user_id}`}
-                                          /> :
-                                          <i className="fal fa-user display-4 text-70 d-block" />
-                                      }
-                                    </Col>
-
-                                    <Col flex="1 1">
-                                      <div className="--details">
-                                        <Row gutter={[0, 5]}>
-                                          <Col span={24} className="--text">
-                                            <div dangerouslySetInnerHTML={ { __html: messageText }} />
-                                          </Col>
-
-                                          <Col span={24} className="--date">
-                                            {config.language === 'fa'
-                                              ? moment.unix(message?.sent_at).format('jDD jMMMM jYYYY - HH:mm')
-                                              : <Moment format="DD MMM, YYYY - HH:mm" unix locale="en">{message?.sent_at}</Moment>
-                                            }
-                                          </Col>
-                                        </Row>
-                                      </div>
-                                    </Col>
-                                  </Row>
-                                </Col>
-                              )
-                            }) :
-                            <Col span={24}><Empty description={t("no_message")}/></Col>
-                          }
-                        </Row>
-                      </Spin>
-                    </Col>
-
-                    <Col span={24} className="--sendMessage">
-                      <Form
-                        className="h-100 --form"
-                        name="sendMessage-form"
-                        form={sendMessageForm}
-                        onFinish={handleSendMessage}
-                      >
-                        <Row>
-                          <Col flex="1 1" className="__messageInput">
-                            <Form.Item
-                              name="message"
-                              noStyle
-                            >
-                              <TextArea
-                                autoSize={{ minRows: 3, maxRows: 3}}
-                                addonAfter=".com"
-                                placeholder={ t(__('Type a message')) }
-                                bordered={false}
-                                allowClear
-                                spellCheck={false}
-                                onChange={e => setMessageText(e.target.value)}
-                              />
-                            </Form.Item>
-                          </Col>
-
-                          <Col className="__messageSendBtn">
-                            <Button type="primary" shape="circle" icon={ <i className="far fa-location-arrow display-4 text-white mr-2 mt-1" /> } htmlType="submit" disabled={!!!messageText} />
-                          </Col>
-                        </Row>
-                      </Form>
-                    </Col>
-                  </Row> :
+                  <Chat
+                    contentRef = {messageContent}
+                    conversationData = {conversationData}
+                    requestMode = {requestMode}
+                    user = {user}
+                    messageScrollBottom = {messageScrollBottom}
+                  /> :
                   <Empty description={t("no_selected_conversation")}/>
               ) :
               (
