193507f19550f82d2cfd7210d56fc19b30bd76ec
/Masoud <masoud_tesna@live.com>
2022-04-20 18:11:39 +0430

    create top section for store details component...

---
+++ src/templates/views/Store.js
@@ -0,0 +1,49 @@
+import {Col, Row} from "antd";
+import {SeoGenerator} from "../../functions/Helper";
+import {useNavigate, useParams} from "react-router-dom";
+import {useGetApi} from "../../functions";
+import {useGetConfig} from "../../contexts/config/ConfigContext";
+import StoreDetails from "./store/StoreDetails";
+
+const Store = () => {
+  
+  // get product path from url:
+  const { company: companySeoName } = useParams();
+  
+  // get initial config:
+  const { config } = useGetConfig();
+  
+  const navigate = useNavigate();
+  
+  // get invoices from API:
+  const { isLoading, data } = useGetApi(
+    `20/Stores`,
+    {
+      company_seo_name: companySeoName
+    },
+    `storeDetails_${companySeoName}`,
+    {
+      enabled: !!companySeoName,
+      refetchOnWindowFocus: false
+    }
+  );
+  
+  const store = data || {};
+  
+  return (
+    <Row>
+      <SeoGenerator
+        title={  companySeoName }
+        description={ store?.company_Introduction?.detailed_company_introduction?.slice(0, 152)+'...' }
+        canonical={ `https://alaedeen.com/store/${companySeoName}` }
+        ogImage={store?.company_Introduction?.logo[config?.language] || ""}
+      />
+      
+      <Col span={24} className="store--container">
+        <StoreDetails store={store} isLoading={isLoading} />
+      </Col>
+    </Row>
+  );
+};
+
+export default Store;
---
+++ src/templates/views/store/StoreDetails.js
@@ -0,0 +1,237 @@
+import "./styles/StoreDetails.less";
+import {Col, Divider, Row, Skeleton, Typography} from "antd";
+import ShowResponsiveImage from "../../common/ShowResponsiveImage";
+import ImageGallery from "../../common/ImageGallery";
+import React, {useEffect, useRef, useState} from "react";
+import {useGetConfig} from "../../../contexts/config/ConfigContext";
+import {useTranslation} from "react-i18next";
+import {__, fn_get_lines_count} from "../../../functions/Helper";
+import {useWindowSize} from "../../../functions";
+
+const StoreDetails = ({store, isLoading}) => {
+  
+  const { Paragraph } = Typography;
+  
+  // get initial config:
+  const { config } = useGetConfig();
+  
+  const { width } = useWindowSize();
+  
+  const { t } = useTranslation();
+  
+  const storeAboutUs = useRef();
+  
+  const [ellipsisStoreAboutUs, setEllipsisStoreAboutUs] = useState(true);
+  const [ellipsisStoreAboutUsShow, setEllipsisStoreAboutUsShow] = useState(false);
+  
+  const storeAboutUsEllipseHandle = () => {
+    setEllipsisStoreAboutUs(prevState => !prevState);
+    if (!ellipsisStoreAboutUs) {
+      return storeAboutUs.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }
+  
+  useEffect(() => {
+    let mountAboutUsSection = true;
+    
+    if (storeAboutUs.current && !isLoading) {
+      const aboutUsLinesCount = fn_get_lines_count(storeAboutUs.current);
+      if (aboutUsLinesCount >= 7 && width >= 993) {
+        setEllipsisStoreAboutUsShow(true);
+      }
+      else if (aboutUsLinesCount >= 4 && width < 993) {
+        setEllipsisStoreAboutUsShow(true);
+      }
+      else {
+        setEllipsisStoreAboutUsShow(false);
+      }
+    }
+    
+    return () => {
+      mountAboutUsSection = false;
+    }
+  }, [storeAboutUs.current, isLoading]);
+  
+  const FactoriesLogo = ({ logo, imageAlt, object_id }) => {
+    
+    // get initial config:
+    const { config } = useGetConfig();
+    
+    const logoSize = width >= 993 ? 50 : 35;
+    
+    if ((config.language === 'en' || config.language === 'ar') && logo?.en) {
+      return (
+        <ShowResponsiveImage
+          imagePath={ logo?.en }
+          imageFolder='company_logo'
+          width={logoSize}
+          height={logoSize}
+          skeletonWidth={ `${logoSize}px` }
+          skeletonHeight={ `${logoSize}px` }
+          skeletonSvgWidth="3rem"
+          skeletonRadius="50%"
+          imageAlt={ imageAlt }
+          object_id={object_id}
+          object_type={`company_logo_en`}
+        />
+      );
+    }
+    
+    if (config.language === 'fa' && logo?.fa) {
+      return (
+        <ShowResponsiveImage
+          imagePath={ logo.fa }
+          imageFolder='company_logo'
+          width={logoSize}
+          height={logoSize}
+          skeletonWidth={ `${logoSize}px` }
+          skeletonHeight={ `${logoSize}px` }
+          skeletonSvgWidth="3rem"
+          skeletonRadius="50%"
+          imageAlt={ imageAlt }
+          object_id={object_id}
+          object_type={`company_logo_fa`}
+        />
+      );
+    }
+    
+    return (
+      <ShowResponsiveImage
+        skeletonWidth={ `${logoSize}px` }
+        skeletonHeight={ `${logoSize}px` }
+        skeletonRadius="50%"
+        skeletonSvgWidth="3rem"
+      />
+    );
+    
+  }
+  
+  return (
+    <Row gutter={[0, 15]} className="storeDetails--container">
+      <Col span={24} className="storeDetails--topSection">
+        <Row gutter={{ md: 17 }}>
+          <Col className="storeDetails--imageContainer">
+            {isLoading ?
+              <ShowResponsiveImage
+                imagePath=""
+                skeletonWidth="100%"
+                skeletonHeight="350px"
+              /> :
+              <ImageGallery images={store?.company_Introduction?.company_images} type="profiles" />
+            }
+          </Col>
+          <Col className="storeDetails">
+            <Row gutter={[0, 20]} className="h-100">
+              <Col span={24}>
+                <Row gutter={[0, 20]}>
+                  <Col className="__top" span={24}>
+                    <Row gutter={10} justify="space-between">
+                      <Col className="--title">
+                        {(isLoading) ?
+                          <Skeleton.Input className="storeSkeleton--text" style={{ "--text-width": "30%", "--text-height": "16px" }} active={true} size={"small"} /> :
+                          <h1>{ store?.general?.company }</h1>
+                        }
+                      </Col>
+                      <Col className="--logo">
+                        <FactoriesLogo
+                          logo={ store?.company_Introduction?.logo }
+                          imageAlt={ store?.general?.company }
+                          object_id={ store?.general?.company_id }
+                          store_type="F"
+                        />
+                      </Col>
+                    </Row>
+                  </Col>
+  
+                  {(isLoading) ?
+                    <>
+                      <Skeleton active={true} paragraph={{ rows: 4 }} />
+                      <div className="px-2 w-100">
+                        <Divider className="border-ef m-0 mt-2" />
+                      </div>
+                    </> :
+                    store?.company_Introduction?.detailed_company_introduction ?
+                      (
+                        <Col span={24} className="--aboutUs" ref={storeAboutUs}>
+                          <Paragraph
+                            ellipsis={
+                              ellipsisStoreAboutUsShow ?
+                                (
+                                  ellipsisStoreAboutUs ?
+                                    {
+                                      rows: width < 993 ? 4 : 10,
+                                    } :
+                                    false
+                                ) :
+                                false
+                            }
+                            className="vv-font-size-2 text-70 font-weight-500"
+                          >
+                            <div dangerouslySetInnerHTML={ { __html: `${store?.company_Introduction?.detailed_company_introduction}` }} />
+                          </Paragraph>
+          
+                          {ellipsisStoreAboutUsShow &&
+                            <div className="__ellipseBtn" onClick={() => storeAboutUsEllipseHandle()}>
+                              {ellipsisStoreAboutUs ?
+                                <>
+                                  <span className="cursor-pointer">{t('more')}</span>
+                                  <i className={ `fas fa-long-arrow-${config.language === 'en' ? 'right' : 'left'} cursor-pointer` } />
+                                </> :
+                                <>
+                                  <i className={ `fas fa-long-arrow-${config.language === 'en' ? 'left' : 'right'} cursor-pointer` } />
+                                  <span className="cursor-pointer">{t('less')}</span>
+                                </>
+                              }
+            
+                            </div>
+                          }
+          
+                          <div className="px-2">
+                            <Divider className="border-ef m-0 mt-2" />
+                          </div>
+                        </Col>
+                      ) :
+                      null
+                  }
+                </Row>
+              </Col>
+              
+              <Col className="__bottom" span={24}>
+                <Row gutter={[10, 10]} justify="space-between">
+                  {isLoading ?
+                   <>
+                     <Col>
+                       <Skeleton.Input className="storeSkeleton--text" style={{ "--text-width": "20px", "--text-height": "16px" }} active={true} size={"small"} />
+                     </Col>
+                     <Col>
+                       <Skeleton.Input className="storeSkeleton--text" style={{ "--text-width": "20px", "--text-height": "16px" }} active={true} size={"small"} />
+                     </Col>
+                   </> :
+                    <>
+                      <Col>
+                        <Row gutter={10}>
+                          <Col>{t("brand")}: </Col>
+                          <Col>{ store?.general?.brand }</Col>
+                        </Row>
+                      </Col>
+  
+                      <Col>
+                        <Row gutter={10}>
+                          <Col>{t("business_type")}: </Col>
+                          <Col>{ store?.general?.business_type }</Col>
+                        </Row>
+                      </Col>
+                    </>
+                  }
+                </Row>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Col>
+      <Col span={24} className="storeDetails--bottomSection"></Col>
+    </Row>
+  );
+};
+
+export default StoreDetails;
