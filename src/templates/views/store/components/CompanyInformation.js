6741e1715596f17e0560b2757adf78afbce4a9af
/Masoud <masoud_tesna@live.com>
2022-04-21 08:54:33 +0430

    create CompanyInformation.js component for show company details table...

---
+++ src/templates/views/store/components/CompanyInformation.js
@@ -0,0 +1,13 @@
+import {Col, Row} from "antd";
+
+const CompanyInformation = ({details, isLoading}) => {
+  return (
+    <Row>
+      <Col span={24} className="companyInformation--container">
+        CompanyInformation
+      </Col>
+    </Row>
+  );
+};
+
+export default CompanyInformation;
