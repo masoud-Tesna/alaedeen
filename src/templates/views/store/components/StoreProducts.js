180d2725b7c52a35c1977f7ddf7fc3533ff79770
/Masoud <masoud_tesna@live.com>
2022-04-22 06:05:29 +0430

    show products

--- src/templates/views/store/components/StoreProducts.js
+++ src/templates/views/store/components/StoreProducts.js
@@ -42,7 +42,7 @@ const StoreProducts = ({ companyId }) => {
       <Col span={24} className="storeProducts--container">
         <Row justify="center">
           
-          { !productsIsLoading ?
+          { productsIsLoading ?
             <Col span={24}>
               <Row gutter={[20, 20]} className="row-cols-2 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5">
                 <SkeletonCategoryMultiColumn
