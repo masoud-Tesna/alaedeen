30450d864e6ad0d00bf8b08df314acc9bafe6cfd
/Masoud <masoud_tesna@live.com>
2022-05-20 12:36:09 +0430

    create search type select box...

--- src/templates/header/default/components/Search.js
+++ src/templates/header/default/components/Search.js
@@ -1,17 +1,44 @@
-import {useState} from "react";
+import {useCallback, useState} from "react";
 import "../styles/Search.less";
-import {Col, Row} from "antd";
+import {Col, Row, Select} from "antd";
+import {useTranslation} from "react-i18next";
 
 const Search = () => {
+  
+  const { Option } = Select;
+  
+  const { t } = useTranslation();
   
   const [searchInput, setSearchInput] = useState("");
   
   const [searchType, setSearchType] = useState("P");
   
+  const SearchTypeSelect = () => {
+    
+    const handleSearchTypeSelect = useCallback(
+      type => {
+        setSearchType(type)
+      },
+      [],
+    );
+    
+    return <Select
+      defaultValue={searchType}
+      className="--searchType"
+      dropdownClassName="--searchType__dropDown"
+      bordered={false}
+      onChange={handleSearchTypeSelect}
+      size="large"
+    >
+      <Option value="P">{t("products")}</Option>
+      <Option value="C">{t("suppliers")}</Option>
+    </Select>
+  };
+  
   return (
     <Row className="search--container" justify="center">
       <Col>
-        Search
+        <SearchTypeSelect />
       </Col>
     </Row>
   );
