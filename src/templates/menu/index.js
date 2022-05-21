4dfcad8b618b758d3ae364b762f8303cd3141537
/Masoud <masoud_tesna@live.com>
2022-05-21 05:17:38 +0430

    create menu component...

---
+++ src/templates/menu/index.js
@@ -0,0 +1,14 @@
+import "./styles/Menu.less";
+import {Col, Row} from "antd";
+
+const Menu = () => {
+  return (
+    <Row className="menu--container">
+      <Col span={24}>
+        Menu
+      </Col>
+    </Row>
+  );
+};
+
+export default Menu;
