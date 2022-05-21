1ab57d3b85105f7cc524c82c01f8cdf58ba399ea
/Masoud <masoud_tesna@live.com>
2022-05-21 05:12:15 +0430

    create new homepage component...

---
+++ src/templates/views/HomePage.js
@@ -0,0 +1,14 @@
+import "./styles/HomePage.less";
+import {Col, Row} from "antd";
+
+const HomePage = () => {
+  return (
+    <Row>
+      <Col span={24} className="homePage--container">
+        HomePage
+      </Col>
+    </Row>
+  );
+};
+
+export default HomePage;
