7e5be540c6619962790797a1e19f6d25ea60f0ed
/Masoud <masoud_tesna@live.com>
2022-05-22 09:08:55 +0430

    create new footer component...

---
+++ src/templates/footer/default/Footer.js
@@ -0,0 +1,14 @@
+import "./styles/Footer.less";
+import {Col, Row} from "antd";
+
+const Footer = () => {
+  return (
+    <Row>
+      <Col span={24} className="footer--container">
+        Footer
+      </Col>
+    </Row>
+  );
+};
+
+export default Footer;
