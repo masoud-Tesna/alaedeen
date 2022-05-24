101bf7ecd54e2999a2ef6e4d768a19eeb05770b8
/Masoud <masoud_tesna@live.com>
2022-05-24 10:50:22 +0430

    clean code Newsletters component...

--- src/templates/footer/default/components/Newsletters.js
+++ src/templates/footer/default/components/Newsletters.js
@@ -14,52 +14,48 @@ const Newsletters = () => {
   const [isOk, setIsOk] = useState(false);
   
   return (
-    <Row justify="center">
-      <Col className="newsletters--container">
-        <Row gutter={[0, 24]}>
-          <Col span={24} className="text-center --caption">
-            {t("invite_newsletters_text")}
-          </Col>
-          <Col span={24} className="text-center">
-            <Form
-              layout="inline"
-              form={newslettersForm}
-              onValuesChange={() => setIsOk(false)}
-              onFinish={v => {
-                console.log(v);
-                setIsOk(true);
-              }}
-              className="ant-row-center --form"
-            >
-              <Form.Item
-                name="user_email" className="__input"
-                rules={[
-                  {
-                    type: "email",
-                    message: t(__("The input is not valid E-mail"))
-                  },
-                  {
-                    required: true,
-                    message: t(__("Please enter your account E-mail"))
-                  }
-                ]}
-                extra={isOk && "Your email was successfully registered"}
-              >
-                <Input
-                  bordered={false}
-                  placeholder={ t(__('your_email')) }
-                />
-                
-              </Form.Item>
-              <Form.Item
-                valuePropName="checked"
-                className="__btn"
-              >
-                <Button type="primary" htmlType="submit">{t("subscribe")}</Button>
-              </Form.Item>
-            </Form>
-          </Col>
-        </Row>
+    <Row justify="center" className="newsletters--container" gutter={[0, 24]}>
+      <Col span={24} className="text-center --caption">
+        {t("invite_newsletters_text")}
+      </Col>
+      <Col span={24} className="text-center">
+        <Form
+          layout="inline"
+          form={newslettersForm}
+          onValuesChange={() => setIsOk(false)}
+          onFinish={v => {
+            console.log(v);
+            setIsOk(true);
+          }}
+          className="ant-row-center --form"
+        >
+          <Form.Item
+            name="user_email" className="__input"
+            rules={[
+              {
+                type: "email",
+                message: t(__("The input is not valid E-mail"))
+              },
+              {
+                required: true,
+                message: t(__("Please enter your account E-mail"))
+              }
+            ]}
+            extra={isOk && "Your email was successfully registered"}
+          >
+            <Input
+              bordered={false}
+              placeholder={ t(__('your_email')) }
+            />
+      
+          </Form.Item>
+          <Form.Item
+            valuePropName="checked"
+            className="__btn"
+          >
+            <Button type="primary" htmlType="submit">{t("subscribe")}</Button>
+          </Form.Item>
+        </Form>
       </Col>
     </Row>
   );
