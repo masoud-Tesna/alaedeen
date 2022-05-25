657505fb97d83e711739bb92afaa5eea1ae9f6d5
/Masoud <masoud_tesna@live.com>
2022-05-25 10:25:46 +0430

    create Requests component...

---
+++ src/templates/views/homePage/Requests.js
@@ -0,0 +1,14 @@
+import "../styles/Requests.less";
+import {Col, Row} from "antd";
+
+const Requests = () => {
+  return (
+    <Row className="requests--container">
+      <Col span={24}>
+        Requests
+      </Col>
+    </Row>
+  );
+};
+
+export default Requests;
