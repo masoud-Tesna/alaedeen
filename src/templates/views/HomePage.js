ad34c3a30155019903f7868db4e146a0497e8cfa
/Masoud <masoud_tesna@live.com>
2022-05-25 10:26:13 +0430

    use Requests component...

--- src/templates/views/HomePage.js
+++ src/templates/views/HomePage.js
@@ -1,11 +1,51 @@
 import "./styles/HomePage.less";
 import {Col, Row} from "antd";
+import {Helmet} from "react-helmet";
+import {SeoGenerator} from "../../functions/Helper";
+import {useTranslation} from "react-i18next";
+import Requests from "./homePage/Requests";
 
 const HomePage = () => {
+  
+  const { t } = useTranslation();
+  
   return (
-    <Row>
-      <Col span={24} className="homePage--container">
-        HomePage
+    <Row className="homePage--container">
+  
+      {/* set schema Organization for home url */}
+      <Helmet>
+        <script type="application/ld+json">
+          {`
+            {
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              "url": "https://alaedeen.com",
+              "logo": "https://alaedeen.com/alaedeen-xs.png"
+            }
+          `}
+        </script>
+      </Helmet>
+  
+      <SeoGenerator
+        title={ t('alaedeen_title') }
+        description={ t('alaedeen_description') }
+        keywords={ t('alaedeen_keywords') }
+      >
+        <script type="application/ld+json">
+          {`
+            {
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              "url": "https://alaedeen.com",
+              "logo": "https://alaedeen.com/alaedeen-xs.png"
+            }
+          `}
+        </script>
+      </SeoGenerator>
+      
+      <Col span={24} className="--requestSection">
+        <div className="--blurBg" />
+        <Requests />
       </Col>
     </Row>
   );
