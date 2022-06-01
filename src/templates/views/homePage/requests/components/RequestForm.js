c2b009a8547c3c3d0c106b444fac10d7ef975f5f
/Masoud <masoud_tesna@live.com>
2022-06-01 09:22:07 +0430

    create RequestForm component...

---
+++ src/templates/views/homePage/requests/components/RequestForm.js
@@ -0,0 +1,14 @@
+import "../../styles/RequestForm.less";
+import {Col, Row} from "antd";
+
+const RequestForm = () => {
+  return (
+    <Row className="requestForm--container">
+      <Col span={24}>
+        RequestForm
+      </Col>
+    </Row>
+  );
+};
+
+export default RequestForm;
