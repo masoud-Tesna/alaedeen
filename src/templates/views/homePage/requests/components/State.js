250973dee70904b6b7e60add8e697a8f266f29c3
/Masoud <masoud_tesna@live.com>
2022-06-09 17:25:11 +0430

    get states from API and show...

--- src/templates/views/homePage/requests/components/State.js
+++ src/templates/views/homePage/requests/components/State.js
@@ -1,29 +1,52 @@
 import "../../styles/State.less";
-import {Col, Row} from "antd";
+import {Col, Row, Skeleton} from "antd";
+import {useGetApiOld} from "../../../../../utilities/functions";
 
 const State = () => {
+  
+  const { isLoading, data } = useGetApiOld(
+    `stats-api`,
+    '',
+    `stats`,
+    {
+      refetchOnWindowFocus: false
+    }
+  );
+  
+  const states = data?.stats || {};
+  
+  console.log(states)
+  
   return (
     <Row className="state--container" align="middle" gutter={26}>
-      <StateCol state="+100" type="Active Suppliers" />
+      <StateCol isLoading={isLoading} state={states?.total_store} type="Active Suppliers" />
       
-      <StateCol state="+100" type="Active Suppliers" />
+      <StateCol isLoading={isLoading} state={states?.total_products} type="total products" />
       
-      <StateCol state="+100" type="Active Suppliers" />
+      <StateCol isLoading={isLoading} state="+100" type="RFQs" />
       
-      <StateCol state="+100" type="Active Suppliers" />
+      <StateCol isLoading={isLoading} state="+100" type="Industries" />
     </Row>
   );
 };
 
-const StateCol = ({ type, state }) => {
+const StateCol = ({ isLoading, type, state }) => {
   return (
     <Col span={6} className="--item">
       <Row align="middle" gutter={8}>
         <Col className="--state">
-          {state}
+          {
+            !!isLoading ?
+              <Skeleton.Input active size={"small"} /> :
+              state
+          }
         </Col>
         <Col className="--type">
-          {type}
+          {
+            !!isLoading ?
+              <Skeleton.Input active size={"small"} /> :
+              type
+          }
         </Col>
       </Row>
     </Col>
