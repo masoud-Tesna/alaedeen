49d29a47e7f29fcb2a4859bb788427e43f67a356
/Masoud <masoud_tesna@live.com>
2022-06-15 19:30:31 +0430

    categories list component Done...

--- src/templates/views/homePage/categories/index.js
+++ src/templates/views/homePage/categories/index.js
@@ -1,27 +1,104 @@
 import "../styles/Categories.less";
-import {Col, Row} from "antd";
+import {Col, Row, Skeleton} from "antd";
+import { Choose, When, Otherwise } from "control-statements";
+import {useGetApiOld} from "../../../../utilities/functions";
+import {useTranslation} from "react-i18next";
+import {Link} from "react-router-dom";
+import SvgIcon from "../../../common/SvgIcon";
+import React from "react";
 
 const Categories = () => {
   
+  const { t } = useTranslation();
+  
+  // get categories from API:
+  const { isLoading, data: categoriesData } = useGetApiOld(
+    `home-categories-api`,
+    '',
+    `allCategories`,
+    {
+      refetchOnWindowFocus: false
+    }
+  );
+  const { categories } = categoriesData || {};
+  
   const CategoryItem = ({category}) => {
     return (
-      <Col span={24}>{category}</Col>
+      <Col span={24} className={`--categoryItem ${category?.p_count === 0 ? 'link--disable': ''}`}>
+        <Row gutter={8} align="middle">
+          <Col className="__image">
+            <img src={ `${process.env.PUBLIC_URL}/categories-image/category-${category?.category_id}.svg` } alt={ category?.category }/>
+          </Col>
+    
+          <Col style={{width: "calc(100% - 40px)"}}>
+            <Row align="middle" justify="space-between">
+              <Col className="__title">
+                {category?.category}
+              </Col>
+              
+              <Col className="__arrow">
+                <SvgIcon icon="angle-right" width={16} height={16} />
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+        
+        <Link to={ `/categories/${category?.seo_name}` } className={ `${category?.p_count === 0 ? 'link--disable': ''}` } />
+      </Col>
+    )
+  }
+  
+  const CategorySkeleton = () => {
+    return (
+      <Col span={24} className="--categoryItem --skeleton">
+        <Row gutter={8} align="middle">
+          <Col className="__image">
+            <Skeleton.Avatar active size="default" shape="circle" />
+          </Col>
+      
+          <Col style={{width: "calc(100% - 40px)"}}>
+            <Row align="middle" justify="space-between">
+              <Col className="__title">
+                <Skeleton.Input style={{ width: `calc(100% - ${Math.floor(Math.random() * 17) + 5}px)`, height: 22 }} active size={"small"} />
+              </Col>
+          
+              <Col className="__arrow">
+                <SvgIcon icon="angle-right" width={16} height={16} />
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Col>
     )
   }
   
   return (
-    <Row className="categories--container" gutter={[0, 30]}>
+    <Row className="categories--container">
       <Col span={24} className="--caption">
-        Categories
+        {t("Categories")}
       </Col>
+  
+      <Choose>
+        <When condition={!!isLoading}>
+          {
+            new Array(8).fill("", 0, 7).map((__, i) => {
+              return (
+                <CategorySkeleton key={i} />
+              )
+            })
+          }
+          <CategorySkeleton />
+        </When>
+        <Otherwise>
+          {
+            categories?.slice(0, 7)?.map(category => {
+              console.log(category)
+              return <CategoryItem key={category?.category_id} category={category} />
+            })
+          }
+        </Otherwise>
+      </Choose>
       
-      <CategoryItem category={'Category 1'} />
-      <CategoryItem category={'Category 2'} />
-      <CategoryItem category={'Category 3'} />
-      <CategoryItem category={'Category 4'} />
-      <CategoryItem category={'Category 5'} />
-      <CategoryItem category={'Category 6'} />
-      <CategoryItem category={'Category 7'} />
     </Row>
   );
 };
