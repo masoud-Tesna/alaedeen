453a91a47f38b731bea75ce286e7c0cfc4fb3a54
/Masoud <masoud_tesna@live.com>
2022-06-16 07:01:34 +0430

    create JoinBox component...

---
+++ src/templates/views/homePage/JoinBox.js
@@ -0,0 +1,14 @@
+import "./styles/JoinBox.less";
+import {Col, Row} from "antd";
+
+const JoinBox = () => {
+  return (
+    <Row className="joinBox--container">
+      <Col span={24}>
+        JoinBox
+      </Col>
+    </Row>
+  );
+};
+
+export default JoinBox;
