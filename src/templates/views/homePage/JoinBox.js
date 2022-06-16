3a1d0abad675a6fb77280edc155c43f49eb34988
/Masoud <masoud_tesna@live.com>
2022-06-16 07:51:36 +0430

    joinBox done... if loading show Skeleton, If the user was logged in show dashboard link, else show join and sign-in link

--- src/templates/views/homePage/JoinBox.js
+++ src/templates/views/homePage/JoinBox.js
@@ -1,11 +1,56 @@
 import "./styles/JoinBox.less";
-import {Col, Row} from "antd";
+import {Col, Row, Skeleton} from "antd";
+import {Link} from "react-router-dom";
+import {useTranslation} from "react-i18next";
+import {useGetAuthState} from "../../../contexts/user/UserContext";
+import { Choose, When, Otherwise } from "control-statements";
 
 const JoinBox = () => {
+  
+  const { t } = useTranslation();
+  
+  const { user_data } = useGetAuthState();
+  
   return (
-    <Row className="joinBox--container">
+    <Row className="joinBox--container" gutter={[0, 35]}>
+      <Col span={24} className="--caption">
+        {t("welcome_to_alaedeen")}
+      </Col>
+      
       <Col span={24}>
-        JoinBox
+        <Row gutter={9} justify="center">
+          <Choose>
+            <When condition={!!user_data?.load}>
+              <Col span={12} className="--joinBox __skeleton">
+                <Skeleton.Input active size={"small"} />
+              </Col>
+  
+              <Col span={12} className="--loginBox __skeleton">
+                <Skeleton.Input active size={"small"} />
+              </Col>
+            </When>
+            
+            <Otherwise>
+              <Choose>
+                <When condition={!(user_data.auth.user_id)}>
+                  <Col span={12} className="--dashboardBox">
+                    <Link to="/dashboard">{t("dashboard")}</Link>
+                  </Col>
+                </When>
+    
+                <Otherwise>
+                  <Col span={12} className="--joinBox">
+                    <Link to="/register">{t("join")}</Link>
+                  </Col>
+      
+                  <Col span={12} className="--loginBox">
+                    <Link to="/sign-in">{t("sign_in")}</Link>
+                  </Col>
+                </Otherwise>
+              </Choose>
+            </Otherwise>
+          </Choose>
+        </Row>
       </Col>
     </Row>
   );
