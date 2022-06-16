15f62a8f0232a30b8e6328efe6003b0fd6b7e521
/Masoud <masoud_tesna@live.com>
2022-06-16 07:53:57 +0430

    create RecommendedProductsBox component...

---
+++ src/templates/views/homePage/RecommendedProductsBox.js
@@ -0,0 +1,14 @@
+import "./styles/RecommendedProductsBox.less";
+import {Col, Row} from "antd";
+
+const RecommendedProductsBox = () => {
+  return (
+    <Row className="recommendedProductsBox--container">
+      <Col span={24}>
+        RecommendedProductsBox
+      </Col>
+    </Row>
+  );
+};
+
+export default RecommendedProductsBox;
