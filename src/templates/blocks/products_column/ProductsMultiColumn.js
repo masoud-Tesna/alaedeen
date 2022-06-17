bc1f85cd9162204d8b41e7eea14c4662c56c478e
/Masoud <masoud_tesna@live.com>
2022-06-17 11:28:29 +0430

    ProductsMultiColumn component Done...

--- src/templates/blocks/products_column/ProductsMultiColumn.js
+++ src/templates/blocks/products_column/ProductsMultiColumn.js
@@ -1,19 +1,30 @@
 import "./styles/ProductsMultiColumn.less";
 import {Col, Row} from "antd";
-
-import productImage from "../../assets/images/product-img.jpg";
 import ShowResponsiveImage from "../../common/ShowResponsiveImage";
 import TextTruncate from "react-text-truncate";
-import {__} from "../../../utilities/functions/Helper";
 
-const ProductsMultiColumn = ({product}) => {
+const ProductsMultiColumn = ({product, imageWidth, imageHeight}) => {
+  
+  const productPrice = parseFloat(product?.price).toFixed(2);
+  const productListPrice = parseFloat(product?.list_price).toFixed(2);
+  const manufacturing_country = product.manufacturing_country;
   
   return (
     <Row className="productsMultiColumn--container" gutter={[0, 8]}>
       <Col className="--topSection align-self-start">
         <Row gutter={[0, 8]}>
           <Col span={24} className="--image">
-            <img src={productImage} alt=""/>
+            <ShowResponsiveImage
+              imagePath={ product?.main_pair?.detailed?.image_path }
+              imageFolder='detailed'
+              width={imageWidth || 214}
+              height={imageHeight || 214}
+              skeletonWidth={imageWidth || "214px"}
+              skeletonHeight={imageHeight || "214px"}
+              imageAlt={ product?.product }
+              object_id={product?.product_id}
+              object_type={`prd`}
+            />
           </Col>
   
           <Col span={24} className="--title">
@@ -21,22 +32,28 @@ const ProductsMultiColumn = ({product}) => {
               line={3}
               element="div"
               truncateText=" …"
-              text="Long Distance Laser Flashlight Military 532NM Green Beam 303 Laser Pointer With Star Cap"
+              text={product?.product}
             />
           </Col>
   
-          <Col span={24} className="--price">
-            US$ 3.60 - 3.80 <span className="__piece">/ Piece</span>
-          </Col>
+          {
+            (productPrice !== "0.00") &&
+            <Col span={24} className="--price">
+              ${ productPrice } {productListPrice !== "0.00" && ` - $${productListPrice}`} {product?.quantity_unit && <span className="__piece">/ { product?.quantity_unit }</span>}
+            </Col>
+          }
   
-          <Col span={24} className="--piece">
-            2 Pieces <span className="__piece"> (MOQ)</span>
-          </Col>
+          {
+            (product?.min_qty && product?.quantity_unit) &&
+              <Col span={24} className="--piece">
+                { product?.min_qty } { product?.quantity_unit } <span className="__piece"> (MOQ)</span>
+              </Col>
+          }
         </Row>
       </Col>
       
       <Col span={24} className="--bottomSection --location align-self-end">
-        <i className={ `fi fi-ir vv-font-size-1-9` } /> <span className="vv-font-size-1-5 text-92">Madrid, Spain</span>
+        <i className={ `fi fi-${ manufacturing_country.toLowerCase() }` } /> <span>{ manufacturing_country }</span>
       </Col>
     </Row>
   );
