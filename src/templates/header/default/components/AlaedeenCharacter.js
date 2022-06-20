14148e2fca759e7f194df9677777f565cd75ad58
/Masoud <masoud_tesna@live.com>
2022-06-20 09:37:05 +0430

    AlaedeenCharacter component RTL style by styled-component...

--- src/templates/header/default/components/AlaedeenCharacter.js
+++ src/templates/header/default/components/AlaedeenCharacter.js
@@ -1,29 +1,38 @@
 import "../styles/AlaedeenCharacter.less";
 // import alaedeen character:
-import alaedeenChar from '../../../assets/images/alaedeen-char.png';
+import {ReactComponent as AlaedeenChar} from '../../../assets/images/alaedeen-char.svg';
 import {Link} from "react-router-dom";
 import {useTranslation} from "react-i18next";
 import {Col, Row} from "antd";
 import {memo} from "react";
+import styled from "styled-components";
+
+import rtl from "styled-components-rtl"
 
 const AlaedeenCharacter = () => {
   
   const { t } = useTranslation();
   
+  const Extra = styled(Col)`
+    ${rtl`
+      padding-left: 5px;
+    `};
+  `;
+  
   return (
-    <Row className="alaedeenCharacter--container">
+    <Row className="alaedeenCharacter--container" align="middle">
       <Col className="--character">
-        <img src={alaedeenChar} alt="Alaedeen.com"/>
+        <AlaedeenChar width={26} />
       </Col>
       
-      <Col className="--extra">
+      <Extra className="--extra">
         <div className="__address">
           <i className="logo-icon-alaedeen-com" />
         </div>
         <div className="__slug">
           { t('alaedeen_slug_section') }
         </div>
-      </Col>
+      </Extra>
       
       <Link to={"/"} className="--link" />
     </Row>
