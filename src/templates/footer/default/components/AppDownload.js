b2701764fbbbd2640632710da9a4356fc17ae3ed
/Masoud <masoud_tesna@live.com>
2022-06-22 13:14:08 +0430

    code splitting and clean code...

--- src/templates/footer/default/components/AppDownload.js
+++ src/templates/footer/default/components/AppDownload.js
@@ -1,43 +1,77 @@
+// style:
 import "../styles/AppDownload.less";
-import alaedeenChar from '../../../assets/images/alaedeen-char.png';
-import appleStore from "../../../assets/images/appleStore.svg";
-import googlePlay from "../../../assets/images/googlePlay.svg";
+
+// antd components:
 import {Col, Row, Space, Tooltip} from "antd";
+
+// styled components:
+import styled from "styled-components";
+import rtl from "styled-components-rtl";
+
+// translation hook:
 import {useTranslation} from "react-i18next";
 
-const AppDownload = () => {
+// icons:
+import {ReactComponent as AlaedeenChar} from '../../../assets/images/alaedeen-char.svg';
+import {ReactComponent as AppleStore} from "../../../assets/images/appleStore.svg";
+import {ReactComponent as GooglePlay} from "../../../assets/images/googlePlay.svg";
+
+const AppDl = styled.div`
+  height: 40px;
+  
+  &,
+  svg {
+    border: 1px solid #FFFFFF;
+    border-radius: 8px;
+  }
+`;
+
+const AppStoreBox = ({Image}) => {
   const { t } = useTranslation();
   
   // Mobile Application Coming Son Tooltip Text:
   const comingSonTooltipText = <span>{ t('coming_son') }</span>;
   
   return (
+    <AppDl>
+      <Tooltip placement="top" title={comingSonTooltipText}>
+        <Image width={130} height={40} />
+      </Tooltip>
+    </AppDl>
+  );
+}
+
+const AppDownload = () => {
+  const { t } = useTranslation();
+  
+  return (
     <Row className="appDownload--container" justify="space-between" align="middle">
       <Col>
-        <Space size={40}>
-          <div className="--character">
-            <img src={alaedeenChar} alt="Alaedeen.com"/>
-          </div>
-          
-          <div className="--caption">
+        <Row gutter={24} align="middle">
+          <Col className="--character">
+            <AlaedeenChar width={24} height={48} />
+          </Col>
+  
+          <Col className="--caption">
             {t("download_application")}
-          </div>
-        </Space>
+          </Col>
+        </Row>
       </Col>
   
       <Col>
-        <Space size={24}>
-          <div className="--download">
-            <Tooltip placement="top" title={comingSonTooltipText}>
-              <img src={ appleStore } alt="_app_store" />
-            </Tooltip>
-          </div>
-          <div className="--download">
-            <Tooltip placement="top" title={comingSonTooltipText}>
-              <img src={ googlePlay } alt="_google_play" />
-            </Tooltip>
-          </div>
-        </Space>
+        <Row gutter={24}>
+          <Col>
+            <AppStoreBox
+              Image={AppleStore}
+            />
+          </Col>
+  
+          <Col>
+            <AppStoreBox
+              Image={GooglePlay}
+            />
+          </Col>
+        </Row>
       </Col>
     </Row>
   );
