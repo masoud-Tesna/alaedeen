86b3b434b8203d0a093e8a700731660c524852b3
/Masoud <masoud_tesna@live.com>
2022-06-22 12:15:29 +0430

    code splitting and clean code...

--- src/templates/footer/default/components/Lists.js
+++ src/templates/footer/default/components/Lists.js
@@ -1,17 +1,204 @@
-import "../styles/Lists.less";
-import {Col, Row, Skeleton, Space} from "antd";
-import {useGetApiOld} from "../../../../utilities/functions";
-import {__} from "../../../../utilities/functions/Helper";
+// Link from react router
 import {Link} from "react-router-dom";
-import React from "react";
+
+// style:
+import "../styles/Lists.less";
+
+// antd components:
+import {Col, Row, Skeleton, Space} from "antd";
+
+// utilities functions:
+import {useGetApiOld} from "../../../../utilities/functions";
+import {__, Else, If, Then} from "../../../../utilities/functions/Helper";
+
+// translation hook:
 import {useTranslation} from "react-i18next";
+
+// config context:
 import {useGetConfig} from "../../../../contexts/config/ConfigContext";
 
-const Lists = () => {
+const SocialList = () => {
+  const { config } = useGetConfig();
   
+  let instagramLink = "https://instagram.com/hornb2b";
+  
+  if (config.language === "fa") instagramLink = "https://instagram.com/hornb2b.ir";
+  else if (config.language === "ar") instagramLink = "https://instagram.com/horn.ar";
+  
+  return (
+    <Col span={24} className="__linkItem --social">
+      <Space size={32}>
+        <div className="footer--bottomSection-socialLink facebook">
+          <a href="#">
+            <i className="fab fa-facebook-f" />
+          </a>
+        </div>
+      
+        <div className="footer--bottomSection-socialLink instagram">
+          <a href={instagramLink} target="_blank" rel="noreferrer">
+            <i className="fab fa-instagram" />
+          </a>
+        </div>
+      
+        <div className="footer--bottomSection-socialLink twitter">
+          <a href="#">
+            <i className="fab fa-twitter" />
+          </a>
+        </div>
+      
+        <div className="footer--bottomSection-socialLink youtube">
+          <a href="#">
+            <i className="fab fa-youtube" />
+          </a>
+        </div>
+      </Space>
+    </Col>
+  );
+}
+
+const ListContent = (
+  {
+    caption = "",
+    isLoading = true,
+    type= "P",
+    links = []
+  }
+) => {
   const { t } = useTranslation();
   
-  const { config } = useGetConfig();
+  return (
+    <Row gutter={[0, 8]}>
+      <Col className="__caption" span={24}>
+        { t(__(caption)) }
+      </Col>
+      
+      <If condition={isLoading}>
+        <Then>
+          <Col span={24} className="text-47">
+            <Skeleton.Input style={{ width: "calc(.5 * 31vw + 50%)", height: "3vh" }} active size={"small"} />
+          </Col>
+          <Col span={24} className="text-47">
+            <Skeleton.Input style={{ width: "calc(.5 * 37vw + 50%)", height: "3vh" }} active size={"small"} />
+          </Col>
+          <Col span={24} className="text-47">
+            <Skeleton.Input style={{ width: "calc(.5 * 20vw + 50%)", height: "3vh" }} active size={"small"} />
+          </Col>
+          <Col span={24} className="text-47">
+            <Skeleton.Input style={{ width: "calc(.5 * 35vw + 50%)", height: "3vh" }} active size={"small"} />
+          </Col>
+        </Then>
+      </If>
+      
+      {/*{isLoading &&
+        <>
+          <Col span={24} className="text-47">
+            <Skeleton.Input style={{ width: "calc(.5 * 31vw + 50%)", height: "3vh" }} active size={"small"} />
+          </Col>
+          <Col span={24} className="text-47">
+            <Skeleton.Input style={{ width: "calc(.5 * 37vw + 50%)", height: "3vh" }} active size={"small"} />
+          </Col>
+          <Col span={24} className="text-47">
+            <Skeleton.Input style={{ width: "calc(.5 * 20vw + 50%)", height: "3vh" }} active size={"small"} />
+          </Col>
+          <Col span={24} className="text-47">
+            <Skeleton.Input style={{ width: "calc(.5 * 35vw + 50%)", height: "3vh" }} active size={"small"} />
+          </Col>
+        </>
+      }*/}
+      
+      <If condition={type === "P"}>
+        <Then>
+          {
+            links?.map(link => {
+              return(
+                <Col key={`footerPage_${link.page_id}`} className="__linkItem" span={24}>
+                  <Link to= {`/page/${link.seo_name}`} className={(!link?.description && link?.page_id !== "30") ? 'link--disable': ''}>
+                    { link.page}
+                  </Link>
+                </Col>
+              );
+            })
+          }
+        </Then>
+      </If>
+      
+      <If condition={type === "C"}>
+        <Then>
+          {
+            links?.slice(0, 4)?.map(link => {
+              return (
+                <Col key={`footerCategories_${link?.category_id}`} className="__linkItem" span={24}>
+                  {/*link: /categories/${category?.seo_name}*/}
+                  <Link to={ `/categories/${link?.seo_name}` } className={link?.p_count === 0 ? 'link--disable': ''}>
+                    { link.category }
+                  </Link>
+                </Col>
+              );
+            })
+          }
+        </Then>
+      </If>
+      
+      <If condition={type === "S"}>
+        <Then>
+          <SocialList />
+        </Then>
+      </If>
+      
+      {/*{type === "P" ?
+        links?.map(link => {
+          return(
+            <Col key={`footerPage_${link.page_id}`} className="__linkItem" span={24}>
+              <Link to= {`/page/${link.seo_name}`} className={(!link?.description && link?.page_id !== "30") ? 'link--disable': ''}>
+                { link.page}
+              </Link>
+            </Col>
+          );
+        }) :
+        type === "C" ?
+          links?.slice(0, 4)?.map(link => {
+            return (
+              <Col key={`footerCategories_${link?.category_id}`} className="__linkItem" span={24}>
+                link: /categories/${category?.seo_name}
+                <Link to={ `/categories/${link?.seo_name}` } className={link?.p_count === 0 ? 'link--disable': ''}>
+                  { link.category }
+                </Link>
+              </Col>
+            );
+          }) :
+          <Col span={24} className="__linkItem --social">
+            <Space size={32}>
+              <div className="footer--bottomSection-socialLink facebook">
+                <a href="#" className="">
+                  <i className="fab fa-facebook-f" />
+                </a>
+              </div>
+              
+              <div className="footer--bottomSection-socialLink instagram">
+                <a className="" href={instagramLink} target="_blank" rel="noreferrer">
+                  <i className="fab fa-instagram" />
+                </a>
+              </div>
+              
+              <div className="footer--bottomSection-socialLink twitter">
+                <a href="#" className="">
+                  <i className="fab fa-twitter" />
+                </a>
+              </div>
+              
+              <div className="footer--bottomSection-socialLink youtube">
+                <a href="#" className="">
+                  <i className="fab fa-youtube" />
+                </a>
+              </div>
+            </Space>
+          </Col>
+      }*/}
+    </Row>
+  )
+}
+
+const Lists = () => {
   
   // get categories from API:
   const { isLoading: categoriesIsLoading, data: categories_data } = useGetApiOld(
@@ -35,102 +222,6 @@ const Lists = () => {
   );
   
   const pages = pages_data || [];
-  
-  const ListContent = (
-    {
-      caption = "",
-      isLoading = true,
-      type= "P",
-      links = []
-    }
-  ) => {
-    
-    let instagramLink = "https://instagram.com/hornb2b";
-    
-    switch (config.language) {
-      case "fa" :
-        instagramLink = "https://instagram.com/hornb2b.ir";
-        break;
-      case "ar" :
-        instagramLink = "https://instagram.com/horn.ar";
-        break;
-    }
-    
-    return (
-      <Row gutter={[0, 8]}>
-        <Col className="__caption" span={24}>
-          { t(__(caption)) }
-        </Col>
-    
-        {isLoading &&
-          <>
-            <Col span={24} className="text-47">
-              <Skeleton.Input style={{ width: "calc(.5 * 31vw + 50%)", height: "3vh" }} active size={"small"} />
-            </Col>
-            <Col span={24} className="text-47">
-              <Skeleton.Input style={{ width: "calc(.5 * 37vw + 50%)", height: "3vh" }} active size={"small"} />
-            </Col>
-            <Col span={24} className="text-47">
-              <Skeleton.Input style={{ width: "calc(.5 * 20vw + 50%)", height: "3vh" }} active size={"small"} />
-            </Col>
-            <Col span={24} className="text-47">
-              <Skeleton.Input style={{ width: "calc(.5 * 35vw + 50%)", height: "3vh" }} active size={"small"} />
-            </Col>
-          </>
-        }
-    
-        {type === "P" ?
-          links?.map(link => {
-            return(
-              <Col key={`footerPage_${link.page_id}`} className="__linkItem" span={24}>
-                <Link to= {`/page/${link.seo_name}`} className={(!link?.description && link?.page_id !== "30") ? 'link--disable': ''}>
-                  { link.page}
-                </Link>
-              </Col>
-            );
-          }) :
-          type === "C" ?
-            links?.slice(0, 4)?.map(link => {
-              return (
-                <Col key={`footerCategories_${link?.category_id}`} className="__linkItem" span={24}>
-                  {/*link: /categories/${category?.seo_name}*/}
-                  <Link to={ `/categories/${link?.seo_name}` } className={link?.p_count === 0 ? 'link--disable': ''}>
-                    { link.category }
-                  </Link>
-                </Col>
-              );
-            }) :
-            <Col span={24} className="__linkItem --social">
-              <Space size={32}>
-                <div className="footer--bottomSection-socialLink facebook">
-                  <a href="#" className="">
-                    <i className="fab fa-facebook-f" />
-                  </a>
-                </div>
-    
-                <div className="footer--bottomSection-socialLink instagram">
-                  <a className="" href={instagramLink} target="_blank" rel="noreferrer">
-                    <i className="fab fa-instagram" />
-                  </a>
-                </div>
-    
-                <div className="footer--bottomSection-socialLink twitter">
-                  <a href="#" className="">
-                    <i className="fab fa-twitter" />
-                  </a>
-                </div>
-    
-                <div className="footer--bottomSection-socialLink youtube">
-                  <a href="#" className="">
-                    <i className="fab fa-youtube" />
-                  </a>
-                </div>
-              </Space>
-            </Col>
-        }
-      </Row>
-    )
-  }
   
   return (
     <Row className="lists--container" >
